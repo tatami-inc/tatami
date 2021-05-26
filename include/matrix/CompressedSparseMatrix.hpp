6bb67599c2cc2978af61cf177da94785d341fb82
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-05-25 21:36:33 -0700

    Fixed assorted bugs with the sparse implementation.

--- include/matrix/CompressedSparseMatrix.hpp
+++ include/matrix/CompressedSparseMatrix.hpp
@@ -29,50 +29,40 @@ public:
 
     const T* get_row(size_t i, T* out_values, size_t first=0, size_t last=-1, workspace* work=NULL) const {
         last = std::min(last, this->ncols);
-
         if constexpr(ROW) {
             get_primary_dimension_expanded(i, first, last, this->ncols, out_values, 0);
         } else {
             get_secondary_dimension_expanded(i, first, last, work, out_values, 0);
         }
-
         return out_values;
     }
 
     const T* get_column(size_t i, T* out_values, size_t first=0, size_t last=-1, workspace* work=NULL) const {
         last = std::min(last, this->nrows);
-        
         if constexpr(ROW) {
             get_secondary_dimension_expanded(i, first, last, work, out_values, 0);
         } else {
             get_primary_dimension_expanded(i, first, last, this->nrows, out_values, 0);
         }
-        
         return out_values;
     }
     
     typename sparse_matrix<T>::sparse_range get_sparse_row(size_t i, T* out_values, IDX* out_indices, size_t first=0, size_t last=-1, workspace* work=NULL) const {
         last = std::min(last, this->ncols);
-
         if constexpr(ROW) {
-            get_primary_dimension_raw(i, first, last, this->ncols, out_values, out_indices);
+            return get_primary_dimension_raw(i, first, last, this->ncols, out_values, out_indices);
         } else {
-            get_secondary_dimension_raw(i, first, last, work, out_values, out_indices); 
+            return get_secondary_dimension_raw(i, first, last, work, out_values, out_indices); 
         }
-
-        return typename sparse_matrix<T>::sparse_range(i, out_values, out_indices);
     }
 
     typename sparse_matrix<T>::sparse_range get_sparse_column(size_t i, T* out_values, IDX* out_indices, size_t first=0, size_t last=-1, workspace* work=NULL) const {
         last = std::min(last, this->nrows);
-
         if constexpr(ROW) {
-            get_secondary_dimension_raw(i, first, last, work, out_values, out_indices); 
+            return get_secondary_dimension_raw(i, first, last, work, out_values, out_indices); 
         } else {
-            get_primary_dimension_raw(i, first, last, this->nrows, out_values, out_indices);
+            return get_primary_dimension_raw(i, first, last, this->nrows, out_values, out_indices);
         }
-
-        return typename sparse_matrix<T>::sparse_range(i, out_values, out_indices);
     }
 
     struct compressed_sparse_workspace : public workspace {
@@ -109,7 +99,7 @@ public:
             } else if (i + 1 == prev_i) {
                 for (size_t c = first; c < last; ++c, ++pIt) {
                     auto& curdex = offsets[c];
-                    if (curdex != *pIt && indices[curdex] >= i) {
+                    if (curdex != *pIt && indices[curdex-1] >= i) {
                         --curdex;
                     }
                 }
@@ -226,21 +216,21 @@ private:
 
     typename sparse_matrix<T>::sparse_range get_primary_dimension_raw(size_t i, size_t first, size_t last, size_t otherdim, T* out_values, IDX* out_indices) const {
         auto obtained = get_primary_dimension(i, first, last, otherdim);
-        typename sparse_matrix<T>::sparse_range output(obtained.first);
+        typename sparse_matrix<T>::sparse_range output(obtained.second);
 
         if constexpr(std::is_convertible<decltype(values.begin()), const T*>::value) {
-            output.value = values.begin() + obtained.second;
+            output.value = values.begin() + obtained.first;
         } else {
-            auto vIt = values.begin() + obtained.second;
-            std::copy(vIt, vIt + obtained.first, out_values);
+            auto vIt = values.begin() + obtained.first;
+            std::copy(vIt, vIt + obtained.second, out_values);
             output.value = out_values;
         }
 
         if constexpr(std::is_convertible<decltype(indices.begin()), const IDX*>::value) {
-            output.index = indices.begin() + obtained.second;
+            output.index = indices.begin() + obtained.first;
         } else {
-            auto iIt = indices.begin() + obtained.second;
-            std::copy(iIt, iIt + obtained.first, out_indices);
+            auto iIt = indices.begin() + obtained.first;
+            std::copy(iIt, iIt + obtained.second, out_indices);
             output.index = out_indices;
         }
 
@@ -250,16 +240,16 @@ private:
     void get_primary_dimension_expanded(size_t i, size_t first, size_t last, size_t otherdim, T* out_values, T empty) const {
         std::fill(out_values, out_values + (last - first), empty);
         auto obtained = get_primary_dimension(i, first, last, otherdim);
-        auto vIt = values.begin() + obtained.second;
-        auto iIt = indices.begin() + obtained.second;
-        for (size_t x = 0; x < obtained.first; ++x, ++vIt, ++iIt) {
-            out_values[*iIt] = *vIt;            
+        auto vIt = values.begin() + obtained.first;
+        auto iIt = indices.begin() + obtained.first;
+        for (size_t x = 0; x < obtained.second; ++x, ++vIt, ++iIt) {
+            out_values[*iIt - first] = *vIt;            
         }
         return;
     }
 
     template<class STORE>
-    void get_secondary_dimension(IDX i, size_t first, size_t last, workspace* work, STORE output) const {
+    void get_secondary_dimension(IDX i, size_t first, size_t last, workspace* work, STORE& output) const {
         if (work == NULL) {
             for (size_t c = first; c < last; ++c) { 
                 auto start = indices.begin() + indptrs[c];
@@ -271,7 +261,7 @@ private:
                 }
             }
         } else {
-            compressed_sparse_workspace& worker = *(reinterpret_cast<compressed_sparse_workspace*>(work));
+            compressed_sparse_workspace& worker = *(dynamic_cast<compressed_sparse_workspace*>(work));
             worker.update_indices(i, first, last);
             auto& new_indptrs = worker.get_offsets();
 
@@ -300,18 +290,19 @@ private:
         }
     };
 
-    size_t get_secondary_dimension_raw(IDX i, size_t first, size_t last, workspace* work, T* out_values, IDX* out_indices) const {
+    typename sparse_matrix<T>::sparse_range get_secondary_dimension_raw(IDX i, size_t first, size_t last, workspace* work, T* out_values, IDX* out_indices) const {
         raw_store store;
         store.out_values = out_values;
         store.out_indices = out_indices;
         get_secondary_dimension(i, first, last, work, store);
-        return store.n;
+        return typename sparse_matrix<T>::sparse_range(store.n, out_values, out_indices);
     }
 
     struct expanded_store {
         T* out_values;
+        size_t first;
         void add(size_t i, T val) {
-            out_values[i] = val;
+            out_values[i - first] = val;
             return;
         }
     };
@@ -320,6 +311,7 @@ private:
         std::fill(out_values, out_values + (last - first), empty);
         expanded_store store;
         store.out_values = out_values;
+        store.first = first;
         get_secondary_dimension(i, first, last, work, store);
         return;
     }
