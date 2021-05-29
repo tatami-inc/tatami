08fcb69ce74e938029a5d41257381812b741dee7
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-05-28 23:43:33 -0700

    Added defaults for sparse_range default constructor.

--- include/utils/sparse_range.hpp
+++ include/utils/sparse_range.hpp
@@ -7,9 +7,9 @@ template <typename T, typename IDX>
 struct sparse_range {
     sparse_range(size_t n, const T* v=NULL, const IDX* i=NULL) : number(n), value(v), index(i) {}
     sparse_range() {}
-    size_t number;
-    const T* value;
-    const IDX* index;
+    size_t number = 0;
+    const T* value = NULL;
+    const IDX* index = NULL;
 };
 
 }
