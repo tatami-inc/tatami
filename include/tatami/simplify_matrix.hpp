aa692b7823e16d42a24085c0a97bc265766cc603
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-05-31 16:20:31 -0700

    Documented the matrix simplifier.

--- include/tatami/simplify_matrix.hpp
+++ include/tatami/simplify_matrix.hpp
@@ -1,22 +1,38 @@
 #ifndef TATAMI_SIMPLIFY_MATRIX_H
 #define TATAMI_SIMPLIFY_MATRIX_H
 
-#include "DenseMatrix.h"
-#include "CompressedSparseMatrix.h"
+#include "DenseMatrix.hpp"
+#include "CompressedSparseMatrix.hpp"
 
 #include <memory>
 #include <vector>
 #include <deque>
 
+/**
+ * @file simplify_matrix.hpp
+ *
+ * Simplify a (possibly delayed) matrix into a dense or compressed sparse format.
+ */
+
 namespace tatami {
 
+/**
+ * @tparam T Type of the values in the matrix.
+ * @tparam IDX Type of index values.
+ *
+ * @param incoming Pointer to a `typed_matrix`, possibly containing delayed operations.
+ * @param row Whether the output matrix should be in a row-based format (i.e., row-major dense or compressed sparse row).
+ * @param sparse Whether the output matrix should be sparse.
+ *
+ * @return A pointer to a new `DenseMatrix` or `CompressedSparseMatrix`, with the same dimensions and type as the matrix referenced by `incoming`.
+ */
 template <typename T, int IDX>
-inline std::shared_ptr<typed_matrix<T, IDX>*> simplify_matrix(std::shared_ptr<const typed_matrix<T, IDX>*> incoming, bool row, bool keep_sparse) {
+inline std::shared_ptr<typed_matrix<T, IDX>*> simplify_matrix(std::shared_ptr<const typed_matrix<T, IDX>*> incoming, bool row, bool sparse) {
     size_t NR = incoming->nrow();
     size_t NC = incoming->ncol();
 
     if (row) {
-        if (keep_sparse && incoming->is_sparse()) {
+        if (sparse) {
             std::deque<T> buffer_v;
             std::deque<IDX> buffer_i;
             std::vector<size_t> indptrs(NC + 1);
@@ -44,7 +60,7 @@ inline std::shared_ptr<typed_matrix<T, IDX>*> simplify_matrix(std::shared_ptr<co
         }
 
     } else {
-        if (keep_sparse && incoming->is_sparse()) {
+        if (sparse) {
             std::deque<T> buffer_v;
             std::deque<IDX> buffer_i;
             std::vector<size_t> indptrs(NC + 1);
@@ -73,6 +89,16 @@ inline std::shared_ptr<typed_matrix<T, IDX>*> simplify_matrix(std::shared_ptr<co
     }
 }
 
+/**
+ * This overload automatically sets the output row-format and sparsity (`sparse`) based on `tatami::matrix::preferred_dimension()` and `tatami::matrix::is_sparse()`, respectively.
+ *
+ * @tparam T Type of the values in the matrix.
+ * @tparam IDX Type of index values.
+ *
+ * @param incoming Pointer to a `tatami::typed_matrix`, possibly containing delayed operations.
+ *
+ * @return A pointer to a new `tatami::DenseMatrix` or `tatami::CompressedSparseMatrix`, with the same dimensions and type as the matrix referenced by `incoming`.
+ */
 template <typename T, int IDX>
 inline std::shared_ptr<typed_matrix<T, IDX>*> simplify_matrix(std::shared_ptr<const typed_matrix<T, IDX>*> incoming) {
     return simplify_matrix(incoming, incoming->preferred_dimension(), incoming->is_sparse());
