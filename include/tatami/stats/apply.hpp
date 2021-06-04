aa2dba44d0facbe970f17976a215d370d5c4f2f3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-06-03 22:54:06 -0700

    Switch to centralized mechanism for handling apply'ing.

---
+++ include/tatami/stats/apply.hpp
@@ -0,0 +1,108 @@
+#ifndef TATAMI_STATS_APPLY_H
+#define TATAMI_STATS_APPLY_H
+
+#include "../base/typed_matrix.hpp"
+
+namespace tatami {
+
+/**
+ * @tparam MARGIN The dimension over which to apply the calculation of statistics, i.e., rows (0) or columns (1).
+ * @tparam T Type of the matrix value, should be summable.
+ * @tparam IDX Type of the row/column indices.
+ * @tparam STAT Class implementing a statistics store.
+ * This should have the `runnable` boolean member, indicating if it is capable of computing a running statistic;
+ * the `sparse` boolean member, indicating if the statistic can be computed sparsely;
+ * and the `direct()` method, to compute the statistics from a dense vector.
+ * If `runnable = true`, there should be `running()` method to compute a running statistic;
+ * and if `sparse = true`, there should be `*_sparse()` equivalents of the other methods.
+ *
+ * @param p Pointer to a `tatami::typed_matrix`.
+ * @param stat Instance of a statistics store.
+ *
+ * @return The `direct()` or `running()` methods in `stat` are invoked to fill up the store with the relevant row- or column-wise statistics.
+ */
+template<int MARGIN, typename T, typename IDX, class STAT>
+inline void apply(const typed_matrix<T, IDX>* p, STAT& stat) {
+    size_t NR = p->nrow(), NC = p->ncol();
+
+    /* One might question why we use MARGIN in the template if we just convert
+     * it to ROW here. This is because 'MARGIN' is used when we're doing
+     * something to the matrix; 'ROW' is used for the representation of the
+     * matrix itself. I'm keeping the distinction clear in the interface.
+     */
+    constexpr bool ROW = (MARGIN == 0);
+
+    const size_t dim = (ROW ? NR : NC);
+    const size_t otherdim = (ROW ? NC : NR);
+
+    /* If we support running calculations AND the preference 
+     * is not consistent with the margin, we give it a shot.
+     */
+    if constexpr(STAT::runnable) {
+        if (p->prefer_rows() != ROW){
+            std::vector<T> obuffer(dim);
+            auto wrk = p->new_workspace(!ROW);
+
+            if constexpr(STAT::sparse) {
+                if (p->sparse()) {
+                    std::vector<IDX> ibuffer(dim);
+                    for (size_t i = 0; i < otherdim; ++i) {
+                        if constexpr(ROW) { // flipped around; remember, we're trying to get the preferred dimension.
+                            auto range = p->sparse_column(i, obuffer.data(), ibuffer.data(), wrk.get());
+                            stat.running(range);
+                        } else {
+                            auto range = p->sparse_row(i, obuffer.data(), ibuffer.data(), wrk.get());
+                            stat.running(range);
+                        }
+                    }
+                    return;
+                }
+            }
+
+            for (size_t i = 0; i < otherdim; ++i) {
+                if constexpr(ROW) { // flipped around, see above.
+                    auto ptr = p->column(i, obuffer.data(), wrk.get());
+                    stat.running(ptr);
+                } else {
+                    auto ptr = p->row(i, obuffer.data(), wrk.get());
+                    stat.running(ptr);
+                }
+            }
+            return;
+        }
+    }
+
+    std::vector<T> obuffer(otherdim);
+    auto wrk = p->new_workspace(ROW);
+
+    if constexpr(STAT::sparse) {
+        if (p->sparse()) {
+            std::vector<IDX> ibuffer(otherdim);
+            for (size_t i = 0; i < dim; ++i) {
+                if constexpr(ROW) {
+                    auto range = p->sparse_row(i, obuffer.data(), ibuffer.data(), wrk.get());
+                    stat.direct(i, range, otherdim);
+                } else {
+                    auto range = p->sparse_column(i, obuffer.data(), ibuffer.data(), wrk.get());
+                    stat.direct(i, range, otherdim);
+                }
+            }
+            return;
+        }
+    }
+
+    for (size_t i = 0; i < dim; ++i) {
+        if constexpr(ROW) {
+            auto ptr = p->row(i, obuffer.data(), wrk.get());
+            stat.direct(i, ptr, otherdim);
+        } else {
+            auto ptr = p->column(i, obuffer.data(), wrk.get());
+            stat.direct(i, ptr, otherdim);
+        }
+    }
+    return;
+}
+
+}
+
+#endif
--- include/tatami/stats/sums.hpp
+++ include/tatami/stats/sums.hpp
@@ -1,7 +1,8 @@
-#ifndef TATAMI_DIMSUM_HPP
-#define TATAMI_DIMSUM_HPP
+#ifndef TATAMI_STATS_SUMS_HPP
+#define TATAMI_STATS_SUMS_HPP
 
 #include "../base/typed_matrix.hpp"
+#include "apply.hpp"
 #include <vector>
 #include <algorithm>
 
@@ -13,89 +14,47 @@
 
 namespace tatami {
 
-/**
- * @tparam ROW Whether to compute the row sums.
- * If false, computes the column sums instead.
- * @tparam T Type of the matrix value, should be summable.
- * @tparam IDX Type of the row/column indices.
- *
- * @param p Pointer to a `tatami::typed_matrix`.
- *
- * @return A vector of length equal to the number of rows (if `ROW = true`) or columns (if `ROW = false`), containing the row or column sums respectively.
- */
-template<bool ROW, typename T, typename IDX>
-inline std::vector<T> sums(const typed_matrix<T, IDX>* p) {
-    size_t NR = p->nrow(), NC = p->ncol();
-    std::vector<T> obuffer(ROW ? NR : NC);
-    auto output = obuffer.begin();
+template<typename T> 
+struct StatsSumHelper {
+    StatsSumHelper(size_t n) : store(n) {}
 
-    // Deciding whether or not to perform row-wise extraction.
-    if (p->prefer_rows()) {
-        std::vector<T> buffer(NC);
-        auto wrk = p->new_workspace(true);
+    static const bool sparse = true;
+    static const bool runnable = true;
 
-        if (p->sparse()) {
-            std::vector<IDX> ibuffer(NC);
-            for (size_t i = 0; i < NR; ++i) {
-                auto range = p->sparse_row(i, buffer.data(), ibuffer.data(), wrk.get());
-                if constexpr(ROW) {
-                    *output = std::accumulate(range.value, range.value + range.number, static_cast<T>(0));
-                    ++output;
-                } else {
-                    for (size_t j = 0; j < range.number; ++j, ++range.index, ++range.value) {
-                        *(output + *range.index) += *range.value;
-                    }
-                }
-            }
-        } else {
-            for (size_t i = 0; i < NR; ++i) {
-                auto ptr = p->row(i, buffer.data(), wrk.get());
-                if constexpr(ROW) {
-                    *output = std::accumulate(ptr, ptr + NC, static_cast<T>(0));
-                    ++output;
-                } else {
-                    auto copy = output;
-                    for (size_t j = 0; j < NC; ++j, ++copy, ++ptr) {
-                        *copy += *ptr;
-                    }
-                }
-            }
-        }
-    } else {
-        std::vector<T> buffer(NR);
-        auto wrk = p->new_workspace(false);
-
-        if (p->sparse()) {
-            std::vector<IDX> ibuffer(NR);
-            for (size_t i = 0; i < NC; ++i) {
-                auto range = p->sparse_column(i, buffer.data(), ibuffer.data(), wrk.get());
-                if constexpr(ROW) {
-                    for (size_t j = 0; j < range.number; ++j, ++range.index, ++range.value) {
-                        *(output + *range.index) += *range.value;
-                    }
-                } else {
-                    *output = std::accumulate(range.value, range.value + range.number, static_cast<T>(0));
-                    ++output;
-                }
-            }
-        } else {
-            for (size_t i = 0; i < NC; ++i) {
-                auto ptr = p->column(i, buffer.data(), wrk.get());
-                if constexpr(ROW) {
-                    auto copy = output;
-                    for (size_t j = 0; j < NR; ++j, ++copy, ++ptr) {
-                        *copy += *ptr;
-                    }
-                } else {
-                    *output = std::accumulate(ptr, ptr + NR, static_cast<T>(0));
-                    ++output;
-                }
-            }
-        }
+    void direct(size_t i, const T* ptr, size_t dim) {
+        store[i] = std::accumulate(ptr, ptr + dim, static_cast<T>(0));
+        return;
     }
 
-    return obuffer;
-}
+    template<typename IDX>
+    void direct(size_t i, const sparse_range<T, IDX>& range, size_t dim) {
+        store[i] = std::accumulate(range.value, range.value + range.number, static_cast<T>(0));
+        return;
+    }
+
+    void running(const T* ptr) {
+        for (auto sIt = store.begin(); sIt != store.end(); ++sIt, ++ptr) {
+            *sIt += *ptr;
+        }
+        return;
+    }
+
+    template<typename IDX>
+    void running(const sparse_range<T, IDX>& range) {
+        auto vptr = range.value;
+        auto iptr = range.index;
+        for (size_t j = 0; j < range.number; ++j, ++iptr, ++vptr) {
+            store[*iptr] += *vptr;
+        }
+        return;
+    }
+
+    std::vector<T> yield() {
+        return store;
+    }
+private:
+    std::vector<T> store;
+};
 
 /**
  * @tparam T Type of the matrix value, should be summable.
@@ -107,7 +66,9 @@ inline std::vector<T> sums(const typed_matrix<T, IDX>* p) {
  */
 template<typename T, typename IDX>
 inline std::vector<T> column_sums(const typed_matrix<T, IDX>* p) {
-    return sums<false, T, IDX>(p);
+    StatsSumHelper<T> stats(p->ncol());
+    apply<1, T, IDX>(p, stats);
+    return stats.yield();
 }
 
 /**
@@ -120,7 +81,9 @@ inline std::vector<T> column_sums(const typed_matrix<T, IDX>* p) {
  */
 template<typename T, typename IDX>
 inline std::vector<T> row_sums(const typed_matrix<T, IDX>* p) {
-    return sums<true, T, IDX>(p);
+    StatsSumHelper<T> stats(p->nrow());
+    apply<0, T, IDX>(p, stats);
+    return stats.yield();
 }
 
 }
