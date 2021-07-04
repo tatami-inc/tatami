662b2c60da631dfa6abcd9e0624dded73e041014
LTLA <LTLA@users.noreply.github.com>
2021-07-04 06:24:07 +0000

    Deploying to gh-pages from @ LTLA/tatami@3e4f0203add9d83977aca15d6e8f62437e2690fc 🚀

--- docs/search/all_c.js
+++ docs/search/all_c.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['value_98',['value',['../classtatami_1_1typed__matrix.html#a9586bf720aae239231559488e9ce7cd4',1,'tatami::typed_matrix::value()'],['../structtatami_1_1stats_1_1MedianHelper.html#a280175566cf5d63793f571a2d455d7d7',1,'tatami::stats::MedianHelper::value()'],['../structtatami_1_1stats_1_1SumHelper.html#a0c26c5bd55e8049c64c7af12a0c55c80',1,'tatami::stats::SumHelper::value()'],['../structtatami_1_1stats_1_1VarianceHelper.html#a0cf3167f20ea137168018c021003b7c3',1,'tatami::stats::VarianceHelper::value()'],['../structtatami_1_1sparse__range.html#ad23d530e0181196804b761942c73d2c2',1,'tatami::sparse_range::value()']]],
+  ['value_98',['value',['../classtatami_1_1typed__matrix.html#a9586bf720aae239231559488e9ce7cd4',1,'tatami::typed_matrix::value()'],['../structtatami_1_1sparse__range.html#ad23d530e0181196804b761942c73d2c2',1,'tatami::sparse_range::value()']]],
   ['variancehelper_99',['VarianceHelper',['../structtatami_1_1stats_1_1VarianceHelper.html',1,'tatami::stats']]],
   ['variances_2ehpp_100',['variances.hpp',['../variances_8hpp.html',1,'']]]
 ];
--- docs/search/typedefs_4.js
+++ docs/search/typedefs_4.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['value_225',['value',['../classtatami_1_1typed__matrix.html#a9586bf720aae239231559488e9ce7cd4',1,'tatami::typed_matrix::value()'],['../structtatami_1_1stats_1_1MedianHelper.html#a280175566cf5d63793f571a2d455d7d7',1,'tatami::stats::MedianHelper::value()'],['../structtatami_1_1stats_1_1SumHelper.html#a0c26c5bd55e8049c64c7af12a0c55c80',1,'tatami::stats::SumHelper::value()'],['../structtatami_1_1stats_1_1VarianceHelper.html#a0cf3167f20ea137168018c021003b7c3',1,'tatami::stats::VarianceHelper::value()']]]
+  ['value_225',['value',['../classtatami_1_1typed__matrix.html#a9586bf720aae239231559488e9ce7cd4',1,'tatami::typed_matrix']]]
 ];
