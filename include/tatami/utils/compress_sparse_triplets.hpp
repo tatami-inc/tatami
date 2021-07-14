eb792b326cfb02efc06d057abc6a21aee280d2e6
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-14 00:52:16 -0700

    Generalized triplet compressor to support variable row/col types.

--- include/tatami/utils/compress_sparse_triplets.hpp
+++ include/tatami/utils/compress_sparse_triplets.hpp
@@ -17,7 +17,8 @@ namespace tatami {
  * @tparam ROW Whether to create a compressed sparse row format.
  * If `false`, the compressed sparse column format is used instead.
  * @tparam U Random-access container for the values.
- * @tparam V Random access container for the indices.
+ * @tparam V Random access container for the row indices.
+ * @tparam W Random access container for the column indices.
  *
  * @param nr Number of rows.
  * @param nc Number of columns.
@@ -31,8 +32,8 @@ namespace tatami {
  * @return `rows`, `cols` and `values` are sorted in-place by the row and column indices (if `ROW = true`) or by the column and row indices (if `ROW = false`).
  * A vector of index pointers is returned with length `nr + 1` (if `ROW = true`) or `nc + 1` (if `ROW = false`).
  */
-template <bool ROW, class U, class V>
-std::vector<size_t> compress_sparse_triplets(size_t nr, size_t nc, U& values, V& rows, V& cols) {
+template <bool ROW, class U, class V, class W>
+std::vector<size_t> compress_sparse_triplets(size_t nr, size_t nc, U& values, V& rows, W& cols) {
     const size_t N = rows.size();
     if (N != cols.size() || values.size() != N) { 
         throw std::runtime_error("'rows', 'cols' and 'values' should have the same length");
@@ -83,9 +84,14 @@ std::vector<size_t> compress_sparse_triplets(size_t nr, size_t nc, U& values, V&
 
     // Collating the indices.
     std::vector<size_t> output(ROW ? nr + 1 : nc + 1);
-    const auto& target = (ROW ? rows : cols);
-    for (auto t : target) {
-        ++(output[t+1]);
+    if constexpr(ROW) {
+        for (auto t : rows) {
+            ++(output[t+1]);
+        } 
+    } else {
+        for (auto t : cols) {
+            ++(output[t+1]);
+        }
     }
     std::partial_sum(output.begin(), output.end(), output.begin());
 
