0cb8cf80093ec2ee6dd723047ae0fb1b6a40e963
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-07-16 19:06:56 -0700

    Streamlined the gzip writing in the tests.

--- tests/src/ext/MatrixMarketGzip.cpp
+++ tests/src/ext/MatrixMarketGzip.cpp
@@ -2,63 +2,8 @@
 
 #include "tatami/ext/MatrixMarket.hpp"
 
-/* Copied verbatim from https://www.zlib.net/zpipe.c */
-
-#include <stdio.h>
-#include <string.h>
-#include <assert.h>
+#include <cstdio>
 #include "zlib.h"
-
-#define CHUNK 16384
-
-int def(FILE *source, FILE *dest, int level) {
-    int ret, flush;
-    unsigned have;
-    z_stream strm;
-    unsigned char in[CHUNK];
-    unsigned char out[CHUNK];
-
-    /* allocate deflate state */
-    strm.zalloc = Z_NULL;
-    strm.zfree = Z_NULL;
-    strm.opaque = Z_NULL;
-    ret = deflateInit2(&strm, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 15 | 16, 8, Z_DEFAULT_STRATEGY);
-    if (ret != Z_OK)
-        return ret;
-
-    /* compress until end of file */
-    do {
-        strm.avail_in = fread(in, 1, CHUNK, source);
-        if (ferror(source)) {
-            (void)deflateEnd(&strm);
-            return Z_ERRNO;
-        }
-        flush = feof(source) ? Z_FINISH : Z_NO_FLUSH;
-        strm.next_in = in;
-
-        /* run deflate() on input until output buffer not full, finish
-           compression if all of source has been read in */
-        do {
-            strm.avail_out = CHUNK;
-            strm.next_out = out;
-            ret = deflate(&strm, flush);    /* no bad return value */
-            assert(ret != Z_STREAM_ERROR);  /* state not clobbered */
-            have = CHUNK - strm.avail_out;
-            if (fwrite(out, 1, have, dest) != have || ferror(dest)) {
-                (void)deflateEnd(&strm);
-                return Z_ERRNO;
-            }
-        } while (strm.avail_out == 0);
-        assert(strm.avail_in == 0);     /* all input will be used */
-
-        /* done when last data in file processed */
-    } while (flush != Z_FINISH);
-    assert(ret == Z_STREAM_END);        /* stream will be complete */
-
-    /* clean up and return */
-    (void)deflateEnd(&strm);
-    return Z_OK;
-}
 
 #include <limits>
 #include <random>
@@ -86,11 +31,17 @@ TEST(MatrixMarketGzip, GzipTest) {
 
     FILE * ihandle = std::fopen(path.c_str(), "rb");
     std::string gzname = path + ".gz";
-    FILE * ohandle = std::fopen(gzname.c_str(), "wb");
-    def(ihandle, ohandle, Z_DEFAULT_COMPRESSION);
+    gzFile ohandle = gzopen(gzname.c_str(), "w");
+    constexpr int CHUNK = 16384;
+    std::vector<unsigned char> in(CHUNK);
+    size_t nread = 0;
+    while ((nread = std::fread(in.data(), 1, CHUNK, ihandle))) {
+        gzwrite(ohandle, in.data(), nread);
+    }
     std::fclose(ihandle);
-    std::fclose(ohandle);
+    gzclose(ohandle);
 
+    // Loading everyone from file.
     auto loaded = tatami::MatrixMarket::load_layered_sparse_matrix(path.c_str());
     auto loaded_gz = tatami::MatrixMarket::load_layered_sparse_matrix_gzip(gzname.c_str());
 
