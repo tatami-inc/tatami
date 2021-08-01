45c4b5e8f51bbafcb91c501aab2cd929b0a15209
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2021-08-01 02:36:41 -0700

    Hardened tests for the variance calculators.

--- tests/src/stats/variances.cpp
+++ tests/src/stats/variances.cpp
@@ -24,8 +24,23 @@ TEST(ComputingDimVariances, RowVariances) {
     auto sparse_row = tatami::convert_to_sparse(dense_row.get(), true);
     auto sparse_column = tatami::convert_to_sparse(dense_row.get(), false);
 
+    // Doing the difference of squares as a quick-and-dirty reference.
     auto ref = tatami::row_variances(dense_row.get());
-    EXPECT_EQ(ref.size(), sparse_nrow);
+    std::vector<double> expected(sparse_nrow), expectedm(sparse_nrow);
+    for (size_t r = 0; r < sparse_nrow; ++r) {
+        for (size_t c = 0; c < sparse_ncol; ++c) {
+            double x = sparse_matrix[c + r * sparse_ncol];
+            expectedm[r] += x;
+            expected[r] += x * x;
+        }
+        expectedm[r] /= sparse_ncol;
+        expected[r] /= sparse_ncol;
+        expected[r] -= expectedm[r] * expectedm[r];
+        expected[r] *= sparse_ncol;
+        expected[r] /= sparse_ncol - 1;
+    }
+    compare_double_vectors(ref, expected);
+
     compare_double_vectors(ref, tatami::row_variances(dense_column.get()));
     compare_double_vectors(ref, tatami::row_variances(sparse_row.get()));
     compare_double_vectors(ref, tatami::row_variances(sparse_column.get()));
@@ -37,8 +52,23 @@ TEST(ComputingDimVariances, ColumnVariances) {
     auto sparse_row = tatami::convert_to_sparse(dense_row.get(), true);
     auto sparse_column = tatami::convert_to_sparse(dense_row.get(), false);
 
+    // Doing the difference of squares as a quick-and-dirty reference.
     auto ref = tatami::column_variances(dense_row.get());
-    EXPECT_EQ(ref.size(), sparse_ncol);
+    std::vector<double> expected(sparse_ncol), expectedm(sparse_ncol);
+    for (size_t c = 0; c < sparse_ncol; ++c) {
+        for (size_t r = 0; r < sparse_nrow; ++r) {
+            double x = sparse_matrix[c + r * sparse_ncol];
+            expectedm[c] += x;
+            expected[c] += x * x;
+        }
+        expectedm[c] /= sparse_nrow;
+        expected[c] /= sparse_nrow;
+        expected[c] -= expectedm[c] * expectedm[c];
+        expected[c] *= sparse_nrow;
+        expected[c] /= sparse_nrow - 1;
+    }
+    compare_double_vectors(ref, expected);
+
     compare_double_vectors(ref, tatami::column_variances(dense_column.get()));
     compare_double_vectors(ref, tatami::column_variances(sparse_row.get()));
     compare_double_vectors(ref, tatami::column_variances(sparse_column.get()));
