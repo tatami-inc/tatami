a85aa1ed1310f2e843e478293194d6b4a722fc50
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-01-14 23:04:34 -0800

    More careful check for sortedness when compressing triplets, to avoid unnecessary allocations for already-sorted inputs.

--- include/tatami/utils/compress_sparse_triplets.hpp
+++ include/tatami/utils/compress_sparse_triplets.hpp
@@ -14,14 +14,38 @@
 namespace tatami {
 
 template<class Primary, class Secondary>
-void order(std::vector<size_t>& indices, const Primary& primary, const Secondary& secondary) {
-    if (std::is_sorted(primary.begin(), primary.end())) {
+int is_ordered(const Primary& primary, const Secondary& secondary) {
+    if (!std::is_sorted(primary.begin(), primary.end())) {
+        return 2;
+    }
+
+    size_t start = 0;
+    while (start < primary.size()) {
+        size_t end = start + 1;
+        while (end < primary.size() && primary[end] == primary[start]) {
+            if (secondary[end] < secondary[end - 1]) {
+                // Quit on first failure; we've seen enough.
+                return 1;
+            }
+            ++end;
+        }
+        start = end;
+    }
+
+    return 0;
+}
+
+template<class Primary, class Secondary>
+void order(int status, std::vector<size_t>& indices, const Primary& primary, const Secondary& secondary) {
+    if (status == 1) {
         size_t start = 0;
         while (start < primary.size()) {
             size_t end = start + 1;
             while (end < primary.size() && primary[end] == primary[start]) {
                 ++end;
             }
+
+            // Checking if this particular run can be skipped.
             if (!std::is_sorted(secondary.begin() + start, secondary.begin() + end)) {
                 std::sort(indices.begin() + start, indices.begin() + end, [&](size_t left, size_t right) -> bool { 
                     return secondary[left] < secondary[right];
@@ -30,7 +54,7 @@ void order(std::vector<size_t>& indices, const Primary& primary, const Secondary
             start = end;
         }
 
-    } else {
+    } else if (status == 2) {
         std::sort(indices.begin(), indices.end(), [&](size_t left, size_t right) -> bool {
             if (primary[left] == primary[right]) {
                 return (secondary[left] < secondary[right]);
@@ -66,37 +90,48 @@ std::vector<size_t> compress_sparse_triplets(size_t nr, size_t nc, U& values, V&
         throw std::runtime_error("'rows', 'cols' and 'values' should have the same length");
     }
 
-    std::vector<size_t> indices(N);
-    for (size_t i = 0; i < N; ++i) {
-        indices[i] = i;
-    }
-
-    // Sorting without duplicating the data.
+    int order_status = 0;
     if constexpr(ROW) {
-        order(indices, rows, cols);
+        order_status = is_ordered(rows, cols);
     } else {
-        order(indices, cols, rows);
+        order_status = is_ordered(cols, rows);
     }
 
-    // Reordering values in place.
-    for (size_t i = 0; i < indices.size(); ++i) {
-        if (indices[i] == -1) {
-            continue;
+    if (order_status != 0) {
+        std::vector<size_t> indices(N);
+        for (size_t i = 0; i < N; ++i) {
+            indices[i] = i;
         }
 
-        size_t current = i, replacement = indices[i];
-        indices[i] = -1;
+        // Sorting without duplicating the data.
+        if constexpr(ROW) {
+            order(order_status, indices, rows, cols);
+        } else {
+            order(order_status, indices, cols, rows);
+        }
 
-        while (replacement != i) {
-            std::swap(rows[current], rows[replacement]);
-            std::swap(cols[current], cols[replacement]);
-            std::swap(values[current], values[replacement]);
+        // Reordering values in place. This (i) saves memory, and (ii) allows
+        // us to work with classes U and V that may not have well-defined copy
+        // constructors (e.g., if they refer to external memory).
+        for (size_t i = 0; i < indices.size(); ++i) {
+            if (indices[i] == -1) {
+                continue;
+            }
 
-            current = replacement;
-            auto next_replacement = indices[replacement]; 
-            indices[replacement] = -1;
-            replacement = next_replacement;
-        } 
+            size_t current = i, replacement = indices[i];
+            indices[i] = -1;
+
+            while (replacement != i) {
+                std::swap(rows[current], rows[replacement]);
+                std::swap(cols[current], cols[replacement]);
+                std::swap(values[current], values[replacement]);
+
+                current = replacement;
+                auto next_replacement = indices[replacement]; 
+                indices[replacement] = -1;
+                replacement = next_replacement;
+            } 
+        }
     }
 
     // Collating the indices.
