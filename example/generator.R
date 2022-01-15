1db681946faaedf5db5294e4bd03c458f73cb569
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-01-14 23:07:03 -0800

    Updated ignore file, removed remaining BioC-wasm references.

--- example/generator.R
+++
@@ -1,9 +0,0 @@
-# This writes a hypothetical SummarizedExperiment's contents into a several binary blobs.
-
-dir.create("colData")
-writeBin(1:26, "colData/stuff", endian="big")
-
-dir.create("assays")
-dir.create('assays/counts')
-writeBin(c(10L, 26L), "assays/counts/dim", endian="big")
-writeBin(500 + 1:260, "assays/counts/values", endian="big")
