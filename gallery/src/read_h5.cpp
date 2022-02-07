e812a14f0c029eac96525c97bff7c9cd40ce78e3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-02-06 21:27:11 -0800

    Added comparison timings for raw HDF5 extraction.

--- gallery/src/read_h5.cpp
+++ gallery/src/read_h5.cpp
@@ -28,7 +28,8 @@ int main(int argc, char* argv[]) {
     std::string path(argv[2]);
 
     std::shared_ptr<tatami::Matrix<double, int> > ptr;
-    if (handle.childObjType(path) == H5O_TYPE_DATASET) {
+    bool is_dense = (handle.childObjType(path) == H5O_TYPE_DATASET);
+    if (is_dense) {
         handle.close();
         ptr.reset(new tatami::HDF5DenseMatrix<double, int, true>(file, path));
     } else {
@@ -42,12 +43,48 @@ int main(int argc, char* argv[]) {
     }
 
     auto start = std::chrono::system_clock::now();
-    auto sums = tatami::column_sums(ptr.get());
-    std::cout << "First sum: " << sums.front() << "\n";
-    std::cout << "Last sum: " << sums.back() << "\n";
-
+    auto sums = tatami::row_sums(ptr.get());
+    auto total = std::accumulate(sums.begin(), sums.end(), 0);
     auto end = std::chrono::system_clock::now();
+
     auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+    std::cout << "Total sum: " << total << "\n";
     std::cout << "Time taken: " << diff.count() << " ms\n";
+
+    // Comparing the time taken to read the entire file in.
+    if (is_dense) {
+        H5::H5File handle(file, H5F_ACC_RDONLY);
+        auto dhandle = handle.openDataSet(path);
+        std::vector<double> values(ptr->nrow() * ptr->ncol());
+
+        auto start = std::chrono::system_clock::now();
+        dhandle.read(values.data(), H5::PredType::NATIVE_DOUBLE);
+        auto val = std::accumulate(values.begin(), values.end(), 0);
+        auto end = std::chrono::system_clock::now();
+
+        auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << "Total sum: " << val << std::endl;
+        std::cout << "Time taken: " << diff.count() << " ms\n";
+    } else {
+        H5::H5File handle(file, H5F_ACC_RDONLY);
+        auto dhandle = handle.openDataSet(path + "/data");
+        auto ihandle = handle.openDataSet(path + "/indices");
+
+        hsize_t n;
+        dhandle.getSpace().getSimpleExtentDims(&n, NULL);
+        std::vector<double> values(n);
+        std::vector<int > index(n);
+
+        auto start = std::chrono::system_clock::now();
+        dhandle.read(values.data(), H5::PredType::NATIVE_DOUBLE);
+        ihandle.read(index.data(), H5::PredType::NATIVE_INT);
+        auto val = std::accumulate(values.begin(), values.end(), 0);
+        auto end = std::chrono::system_clock::now();
+
+        auto diff = std::chrono::duration_cast<std::chrono::milliseconds>(end - start);
+        std::cout << "Total sum: " << val << std::endl;
+        std::cout << "Time taken: " << diff.count() << " ms\n";
+    }
+
     return 0;
 }
