29d43f44a3e6571bb83c92ff0b3c0935eb7540dd
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-03-24 04:19:35 -0700

    Streamlined MM readers by switching to the new byteme libary.

--- include/tatami/ext/MatrixMarket.hpp
+++ include/tatami/ext/MatrixMarket.hpp
@@ -11,11 +11,12 @@
 #include "../base/CompressedSparseMatrix.hpp"
 #include "../utils/compress_sparse_triplets.hpp"
 
-#include "buffin/parse_text_file.hpp"
+#include "byteme/RawFileReader.hpp"
+#include "byteme/RawBufferReader.hpp"
 
 #ifdef TATAMI_USE_ZLIB
-#include "buffin/parse_gzip_file.hpp"
-#include "buffin/parse_zlib_buffer.hpp"
+#include "byteme/GzipFileReader.hpp"
+#include "byteme/ZlibBufferReader.hpp"
 #endif
 
 /**
@@ -97,69 +98,73 @@ private:
     }
 
 public:
-    template<typename B, class Store>
-    void add(const B* buffer, size_t n, Store& store) {
-        size_t i = 0;
-        while (i < n) {
-            if (buffer[i] == '%') {
-                in_comment = true;
+    template<class Reader, class Store>
+    void operator()(Reader& reader, Store& store) {
+        bool remaining;
 
-                // Try to get quickly to the next line.
-                do {
+        do {
+            remaining = reader();
+            auto buffer = reinterpret_cast<const char*>(reader.buffer());
+            auto n = reader.available();
+
+            size_t i = 0;
+            while (i < n) {
+                if (buffer[i] == '%') {
+                    in_comment = true;
+
+                    // Try to get quickly to the next line.
+                    do {
+                        ++i;
+                    } while (i < n && buffer[i] != '\n');
+                } else if (buffer[i] == '\n') {
+                    new_line(store);
                     ++i;
-                } while (i < n && buffer[i] != '\n');
-            } else if (buffer[i] == '\n') {
-                new_line(store);
-                ++i;
-            } else if (in_comment) {
-                // Try to get to the next line, again.
-                do {
-                    ++i;
-                } while (i < n && buffer[i] != '\n');
-            } else {
-                while (i < n) {
-                    if (!std::isdigit(buffer[i])) {
-                        if (!std::isspace(buffer[i])) {
-                            throw std::runtime_error("values should be non-negative integers on line " + std::to_string(current_line));
+                } else if (in_comment) {
+                    // Try to get to the next line, again.
+                    do {
+                        ++i;
+                    } while (i < n && buffer[i] != '\n');
+                } else {
+                    while (i < n) {
+                        if (!std::isdigit(buffer[i])) {
+                            if (!std::isspace(buffer[i])) {
+                                throw std::runtime_error("values should be non-negative integers on line " + std::to_string(current_line));
+                            }
+
+                            if (buffer[i] == '\n') {
+                                break;
+                            }
+
+                            if (non_empty) {
+                                ++onto;
+                                non_empty = false;
+                            }
+                            ++i;
+                            continue;
                         }
 
-                        if (buffer[i] == '\n') {
-                            break;
-                        }
-
-                        if (non_empty) {
-                            ++onto;
-                            non_empty = false;
+                        non_empty = true; // check that something was _actually_ present in this field.
+                        int delta = buffer[i] - '0';
+                        switch(onto) {
+                            case 0:
+                                currow *= 10;
+                                currow += delta;
+                                break;
+                            case 1:
+                                curcol *= 10;
+                                curcol += delta;
+                                break;
+                            case 2:
+                                curval *= 10;
+                                curval += delta;
+                                break;
                         }
                         ++i;
-                        continue;
                     }
-
-                    non_empty = true; // check that something was _actually_ present in this field.
-                    int delta = buffer[i] - '0';
-                    switch(onto) {
-                        case 0:
-                            currow *= 10;
-                            currow += delta;
-                            break;
-                        case 1:
-                            curcol *= 10;
-                            curcol += delta;
-                            break;
-                        case 2:
-                            curval *= 10;
-                            curval += delta;
-                            break;
-                    }
-                    ++i;
                 }
             }
-        }
-        return;
-    }
+        } while (remaining);
 
-    template<class Store>
-    void finish(Store& store) {
         // If onto = 0 and non_empty = false, we ended on a newline, so 
         // there's no extra entry to add. Otherwise, we try to add the 
         // last line that was _not_ terminated by a newline.
@@ -186,91 +191,94 @@ public:
  */
 template<typename T, typename IDX> 
 struct SimpleBuilder {
-public:
-    std::vector<uint16_t> short_rows;
-    std::vector<IDX> long_rows;
-    bool use_short_rows = false;
+    struct Core {
+        std::vector<uint16_t> short_rows;
+        std::vector<IDX> long_rows;
+        bool use_short_rows = false;
 
-    std::vector<uint16_t> short_cols;
-    std::vector<IDX> long_cols;
-    bool use_short_cols = false;
+        std::vector<uint16_t> short_cols;
+        std::vector<IDX> long_cols;
+        bool use_short_cols = false;
 
-    size_t nrows, ncols;
-    std::vector<T> values;
+        size_t nrows, ncols;
+        std::vector<T> values;
 
-public:
-    void setdim(size_t currow, size_t curcol, size_t nlines) {
-        nrows = currow;
-        ncols = curcol;
+        void setdim(size_t currow, size_t curcol, size_t nlines) {
+            nrows = currow;
+            ncols = curcol;
 
-        constexpr size_t max16 = std::numeric_limits<uint16_t>::max();
-        if (nrows <= max16) {
-            short_rows.resize(nlines);
-            use_short_rows = true;
-        } else {
-            long_rows.resize(nlines);
+            constexpr size_t max16 = std::numeric_limits<uint16_t>::max();
+            if (nrows <= max16) {
+                short_rows.resize(nlines);
+                use_short_rows = true;
+            } else {
+                long_rows.resize(nlines);
+            }
+
+            if (ncols <= max16) {
+                short_cols.resize(nlines);
+                use_short_cols = true;
+            } else {
+                long_cols.resize(nlines);
+            }
+
+            values.resize(nlines);
         }
 
-        if (ncols <= max16) {
-            short_cols.resize(nlines);
-            use_short_cols = true;
-        } else {
-            long_cols.resize(nlines);
+        void addline(size_t row, size_t col, size_t val, size_t line) {
+            if (use_short_rows) {
+                short_rows[line] = row;
+            } else {
+                long_rows[line] = row;
+            }
+
+            if (use_short_cols) {
+                short_cols[line] = col;
+            } else {
+                long_cols[line] = col;
+            }
+
+            values[line] = val;
         }
+    };
 
-        values.resize(nlines);
-    }
-
-    void addline(size_t row, size_t col, size_t val, size_t line) {
-        if (use_short_rows) {
-            short_rows[line] = row;
-        } else {
-            long_rows[line] = row;
-        }
-
-        if (use_short_cols) {
-            short_cols[line] = col;
-        } else {
-            long_cols[line] = col;
-        }
-
-        values[line] = val;
-    }
-
-public:
-    BaseMMParser base;
-
-    template<typename B>
-    void add(const B* buffer, size_t n) {
-        base.add(buffer, n, *this);
-        return;
-    }
-
-    std::shared_ptr<tatami::Matrix<T, IDX> > finish() {
-        base.finish(*this);
+    template<class Reader>
+    static std::shared_ptr<tatami::Matrix<T, IDX> > build(Reader& reader) {
+        BaseMMParser parser;
+        Core store;
+        parser(reader, store);
 
         auto create_matrix = [&](auto& rows, auto& cols) -> auto {
-            auto idptrs = compress_sparse_triplets<false>(nrows, ncols, values, rows, cols);
+            auto idptrs = compress_sparse_triplets<false>(store.nrows, store.ncols, store.values, rows, cols);
 
             // Jesus, so much typedefing.
             typedef typename std::remove_reference<decltype(rows)>::type RowType;
             typedef typename std::remove_reference<decltype(idptrs)>::type IndType;
-            typedef CompressedSparseColumnMatrix<T, IDX, decltype(values), RowType, IndType> SparseMat;
+            typedef CompressedSparseColumnMatrix<T, IDX, decltype(store.values), RowType, IndType> SparseMat;
 
-            return std::shared_ptr<tatami::Matrix<T, IDX> >(new SparseMat(nrows, ncols, std::move(values), std::move(rows), std::move(idptrs), false));
+            return std::shared_ptr<tatami::Matrix<T, IDX> >(
+                new SparseMat(
+                    store.nrows, 
+                    store.ncols, 
+                    std::move(store.values), 
+                    std::move(rows), 
+                    std::move(idptrs), 
+                    false
+                )
+            );
         };
 
-        if (use_short_rows) {
-            if (use_short_cols) {
-                return create_matrix(short_rows, short_cols);
+        if (store.use_short_rows) {
+            if (store.use_short_cols) {
+                return create_matrix(store.short_rows, store.short_cols);
             } else {
-                return create_matrix(short_rows, long_cols);
+                return create_matrix(store.short_rows, store.long_cols);
             }
         } else {
-            if (use_short_cols) {
-                return create_matrix(long_rows, short_cols);
+            if (store.use_short_cols) {
+                return create_matrix(store.long_rows, store.short_cols);
             } else {
-                return create_matrix(long_rows, long_cols);
+                return create_matrix(store.long_rows, store.long_cols);
             }
         }
     }
@@ -296,9 +304,8 @@ public:
  */
 template<typename T = double, typename IDX = int>
 std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix(const char * filepath, size_t bufsize = 65536) {
-    SimpleBuilder<T, IDX> build;
-    buffin::parse_text_file(filepath, build, bufsize);
-    return build.finish();
+    byteme::RawFileReader reader(filepath, bufsize);
+    return SimpleBuilder<T, IDX>::build(reader);
 }
 
 /**
@@ -310,15 +317,13 @@ std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix(const char * filepat
  *
  * @tparam T Type of value in the `tatami::Matrix` interface.
  * @tparam IDX Integer type for the index.
- * @tparam B Type of the buffer, usually `char` or `unsigned char`.
  *
  * This is equivalent to `load_sparse_matrix()` but assumes that the entire file has been read into `buffer`.
  */
-template<typename T = double, typename IDX = int, typename B>
-std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix_from_buffer(const B* buffer, size_t n) {
-    SimpleBuilder<T, IDX> build;
-    build.add(buffer, n);
-    return build.finish();
+template<typename T = double, typename IDX = int>
+std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix_from_buffer(const unsigned char* buffer, size_t n) {
+    byteme::RawBufferReader reader(buffer, n);
+    return SimpleBuilder<T, IDX>::build(reader);
 }
 
 #ifdef TATAMI_USE_ZLIB
@@ -328,7 +333,7 @@ std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix_from_buffer(const B*
  * The file should contain non-negative integer data in the coordinate format, stored with Gzip compression.
  * @param bufsize Size of the buffer to use for decompression, in bytes.
  *
- * @return A `LayeredMatrixData` object.
+ * @return A pointer to a `tatami::Matrix` object.
  *
  * @tparam T Type of value in the `tatami::Matrix` interface.
  * @tparam IDX Integer type for the index.
@@ -338,9 +343,8 @@ std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix_from_buffer(const B*
  */
 template<typename T = double, typename IDX = int>
 std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix_gzip(const char * filepath, size_t bufsize = 65536) {
-    SimpleBuilder<T, IDX> build;
-    buffin::parse_gzip_file(filepath, build, bufsize);
-    return build.finish();
+    byteme::GzipFileReader reader(filepath, bufsize);
+    return SimpleBuilder<T, IDX>::build(reader);
 }
 
 /**
@@ -349,7 +353,7 @@ std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix_gzip(const char * fi
  * @param n Size of the `buffer` array.
  * @param bufsize Size of the buffer to use for decompression, in bytes.
  *
- * @return A `LayeredMatrixData` object.
+ * @return A pointer to a `tatami::Matrix` object.
  *
  * @tparam T Type of value in the `tatami::Matrix` interface.
  * @tparam IDX Integer type for the index.
@@ -358,11 +362,9 @@ std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix_gzip(const char * fi
  */
 template<typename T = double, typename IDX = int>
 std::shared_ptr<tatami::Matrix<T, IDX> > load_sparse_matrix_from_buffer_gzip(const unsigned char * buffer, size_t n, size_t bufsize = 65536) {
-    SimpleBuilder<T, IDX> build;
-    buffin::parse_zlib_buffer(const_cast<unsigned char*>(buffer), n, build, 3, bufsize);
-    return build.finish();
+    byteme::ZlibBufferReader reader(buffer, n, 3, bufsize);
+    return SimpleBuilder<T, IDX>::build(reader);
 }
-
 
 #endif
 
