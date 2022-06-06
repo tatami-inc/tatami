8bfcbc5c0e9750c0f71b96a2e636f697c741a965
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-06-06 20:46:45 +0000

    Deploying to gh-pages from @ LTLA/tatami@08886d11192e6429e328330f01bc06c5b4a3b37f 🚀

--- docs/search/functions_0.js
+++ docs/search/functions_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['arrayview_0',['ArrayView',['../classtatami_1_1ArrayView.html#a183bb27de4abe81292ecdd7be2633516',1,'tatami::ArrayView']]]
+  ['apply_0',['apply',['../apply_8hpp.html#a794620bd9c24e6c2bc39f4c65d7f1a16',1,'tatami']]],
+  ['arrayview_1',['ArrayView',['../classtatami_1_1ArrayView.html#a183bb27de4abe81292ecdd7be2633516',1,'tatami::ArrayView']]]
 ];
