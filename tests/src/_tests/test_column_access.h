63985f032edbf7145eeb21f2d366c3e9b901c081
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-06-12 02:24:40 -0700

    Testfix to actually test for reverse consecutive access. Added tests for bidirectional (i.e., non-consecutive) access patterns.

--- tests/src/_tests/test_column_access.h
+++ tests/src/_tests/test_column_access.h
@@ -10,26 +10,40 @@ void test_simple_column_access(const Matrix* ptr, const Matrix2* ref, bool forwa
     ASSERT_EQ(NC, ref->ncol());
 
     auto wrk = ptr->new_workspace(false);
+    auto wrk_bi = ptr->new_workspace(false);
+
     for (size_t i = 0; i < NC; i += jump) {
         size_t c = (forward ? i : NC - i - 1);
 
-        auto expected = ref->column(i);
+        auto expected = ref->column(c);
         {
-            auto observed = ptr->column(i);
+            auto observed = ptr->column(c);
             EXPECT_EQ(expected, observed);
 
             // Now with a workspace.
-            auto observedW = ptr->column(i, wrk.get());
+            auto observedW = ptr->column(c, wrk.get());
             EXPECT_EQ(expected, observedW);
         }
 
         {
-            auto observed = ptr->sparse_column(i);
+            auto observed = ptr->sparse_column(c);
             EXPECT_EQ(expected, expand(observed, NR));
 
             // Now with a workspace.
-            auto observedW = ptr->sparse_column(i, wrk.get());
+            auto observedW = ptr->sparse_column(c, wrk.get());
             EXPECT_EQ(expected, expand(observedW, NR));
+        }
+
+        // Check workspace caching when access is bidirectional,
+        // i.e., not purely increasing or decreasing.
+        if (jump > 1 && i) {
+            auto observed = ptr->column(c, wrk_bi.get());
+            EXPECT_EQ(expected, observed);
+
+            auto subc = (forward ? c-1 : c+1);
+            auto observedm1 = ptr->column(subc, wrk_bi.get());
+            auto expectedm1 = ref->column(subc);
+            EXPECT_EQ(expectedm1, observedm1);
         }
     }
 }
@@ -42,28 +56,42 @@ void test_sliced_column_access(const Matrix* ptr, const Matrix2* ref, bool forwa
     ASSERT_EQ(NC, ref->ncol());
 
     auto wrk = ptr->new_workspace(false);
+    auto wrk_bi = ptr->new_workspace(false);
+
     for (size_t i = 0; i < NC; i += jump, first += shift) {
         size_t c = (forward ? i : NC - i - 1);
         auto interval = wrap_intervals(first, first + len, NR);
         size_t start = interval.first, end = interval.second;
-        
-        auto expected = ref->column(i, start, end);
+
+        auto expected = ref->column(c, start, end);
         {
-            auto observed = ptr->column(i, start, end);
+            auto observed = ptr->column(c, start, end);
             EXPECT_EQ(expected, observed);
 
             // Now with a workspace.
-            auto observedW = ptr->column(i, start, end, wrk.get());
+            auto observedW = ptr->column(c, start, end, wrk.get());
             EXPECT_EQ(expected, observedW);
         }
 
         {
-            auto observed = ptr->sparse_column(i, start, end);
+            auto observed = ptr->sparse_column(c, start, end);
             EXPECT_EQ(expected, expand(observed, start, end));
 
             // Now with a workspace.
-            auto observedW = ptr->sparse_column(i, start, end, wrk.get());
+            auto observedW = ptr->sparse_column(c, start, end, wrk.get());
             EXPECT_EQ(expected, expand(observedW, start, end));
+        }
+
+        // Check workspace caching when access is bidirectional,
+        // i.e., not purely increasing or decreasing.
+        if (jump > 1 && i) {
+            auto observed = ptr->column(c, start, end, wrk_bi.get());
+            EXPECT_EQ(expected, observed);
+
+            auto subc = (forward ? c-1 : c+1);
+            auto observedm1 = ptr->column(subc, start, end, wrk_bi.get());
+            auto expectedm1 = ref->column(subc, start, end);
+            EXPECT_EQ(expectedm1, observedm1);
         }
     }
 }
--- tests/src/_tests/test_row_access.h
+++ tests/src/_tests/test_row_access.h
@@ -10,26 +10,40 @@ void test_simple_row_access(const Matrix* ptr, const Matrix2* ref, bool forward 
     ASSERT_EQ(NC, ref->ncol());
 
     auto wrk = ptr->new_workspace(true);
-    for (size_t i = 0; i < NR; i += jump) {
-        size_t c = (forward ? i : NR - i - 1);
+    auto wrk_bi = ptr->new_workspace(true);
 
-        auto expected = ref->row(i);
+    for (size_t i = 0; i < NR; i += jump) {
+        size_t r = (forward ? i : NR - i - 1);
+
+        auto expected = ref->row(r);
         {
-            auto observed = ptr->row(i);
+            auto observed = ptr->row(r);
             EXPECT_EQ(expected, observed);
 
             // Now with a workspace.
-            auto observedW = ptr->row(i, wrk.get());
+            auto observedW = ptr->row(r, wrk.get());
             EXPECT_EQ(expected, observedW);
         }
 
         {
-            auto observed = ptr->sparse_row(i);
+            auto observed = ptr->sparse_row(r);
             EXPECT_EQ(expected, expand(observed, NC));
 
             // Now with a workspace.
-            auto observedW = ptr->sparse_row(i, wrk.get());
+            auto observedW = ptr->sparse_row(r, wrk.get());
             EXPECT_EQ(expected, expand(observedW, NC));
+        }
+
+        // Check workspace caching when access is bidirectional,
+        // i.e., not purely increasing or decreasing.
+        if (jump > 1 && i) {
+            auto observed = ptr->row(r, wrk_bi.get());
+            EXPECT_EQ(expected, observed);
+
+            auto subr = (forward ? r-1 : r+1);
+            auto observedm1 = ptr->row(subr, wrk_bi.get());
+            auto expectedm1 = ref->row(subr);
+            EXPECT_EQ(expectedm1, observedm1);
         }
     }
 }
@@ -42,28 +56,42 @@ void test_sliced_row_access(const Matrix* ptr, const Matrix2* ref, bool forward,
     ASSERT_EQ(NC, ref->ncol());
 
     auto wrk = ptr->new_workspace(true);
+    auto wrk_bi = ptr->new_workspace(true);
+
     for (size_t i = 0; i < NR; i += jump, first += shift) {
-        size_t c = (forward ? i : NR - i - 1);
+        size_t r = (forward ? i : NR - i - 1);
         auto interval = wrap_intervals(first, first + len, NC);
         size_t start = interval.first, end = interval.second;
-        
-        auto expected = ref->row(i, start, end);
+
+        auto expected = ref->row(r, start, end);
         {
-            auto observed = ptr->row(i, start, end);
+            auto observed = ptr->row(r, start, end);
             EXPECT_EQ(expected, observed);
 
             // Now with a workspace.
-            auto observedW = ptr->row(i, start, end, wrk.get());
+            auto observedW = ptr->row(r, start, end, wrk.get());
             EXPECT_EQ(expected, observedW);
         }
 
         {
-            auto observed = ptr->sparse_row(i, start, end);
+            auto observed = ptr->sparse_row(r, start, end);
             EXPECT_EQ(expected, expand(observed, start, end));
 
             // Now with a workspace.
-            auto observedW = ptr->sparse_row(i, start, end, wrk.get());
+            auto observedW = ptr->sparse_row(r, start, end, wrk.get());
             EXPECT_EQ(expected, expand(observedW, start, end));
+        }
+
+        // Check workspace caching when access is bidirectional,
+        // i.e., not purely increasing or decreasing.
+        if (jump > 1 && i) {
+            auto observed = ptr->row(r, start, end, wrk_bi.get());
+            EXPECT_EQ(expected, observed);
+
+            auto subr = (forward ? r-1 : r+1);
+            auto observedm1 = ptr->row(subr, start, end, wrk_bi.get());
+            auto expectedm1 = ref->row(subr, start, end);
+            EXPECT_EQ(expectedm1, observedm1);
         }
     }
 }
