cc7ea1c8d305dca00c80a8581b448e68ee8b72a3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-07-12 22:52:06 +0000

    Deploying to gh-pages from @ LTLA/tatami@a6f8d37763df5cb39a97033cb11f3ead7e09a93b 🚀

--- docs/search/all_4.js
+++ docs/search/all_4.js
@@ -1,4 +1,6 @@
 var searchData=
 [
-  ['end_0',['end',['../classtatami_1_1ArrayView.html#a904e6d03b59a20f2cc7831fbb318cf33',1,'tatami::ArrayView::end()'],['../structtatami_1_1SomeNumericArray.html#aaf5d7921f2356a9a3aca745723611d71',1,'tatami::SomeNumericArray::end()'],['../classtatami_1_1NakedArray.html#a92d1284a06621c798dd3727816b69e83',1,'tatami::NakedArray::end()']]]
+  ['end_0',['end',['../classtatami_1_1ArrayView.html#a904e6d03b59a20f2cc7831fbb318cf33',1,'tatami::ArrayView::end()'],['../structtatami_1_1SomeNumericArray.html#aaf5d7921f2356a9a3aca745723611d71',1,'tatami::SomeNumericArray::end()'],['../classtatami_1_1NakedArray.html#a92d1284a06621c798dd3727816b69e83',1,'tatami::NakedArray::end()']]],
+  ['extract_5fheader_5ffrom_5fbuffer_1',['extract_header_from_buffer',['../MatrixMarket_8hpp.html#ae0ced3181c20a3281a2ab4844b978313',1,'tatami::MatrixMarket']]],
+  ['extract_5fheader_5ffrom_5ffile_2',['extract_header_from_file',['../MatrixMarket_8hpp.html#afc195d253d64d0a67216cfd6d52e8639',1,'tatami::MatrixMarket']]]
 ];
--- docs/search/variables_2.js
+++ docs/search/variables_2.js
@@ -1,4 +1,7 @@
 var searchData=
 [
-  ['number_0',['number',['../structtatami_1_1SparseRange.html#a9116bc26b0d7526e8f211be8a6a4472f',1,'tatami::SparseRange']]]
+  ['ncol_0',['ncol',['../structtatami_1_1MatrixMarket_1_1HeaderDetails.html#a35de5188ab362aaa52aa39d2cfd79188',1,'tatami::MatrixMarket::HeaderDetails']]],
+  ['nlines_1',['nlines',['../structtatami_1_1MatrixMarket_1_1HeaderDetails.html#a63af108a27977c4e626181e58f28cb95',1,'tatami::MatrixMarket::HeaderDetails']]],
+  ['nrow_2',['nrow',['../structtatami_1_1MatrixMarket_1_1HeaderDetails.html#a5cdfdb10078f2f25cae2f2aa22279007',1,'tatami::MatrixMarket::HeaderDetails']]],
+  ['number_3',['number',['../structtatami_1_1SparseRange.html#a9116bc26b0d7526e8f211be8a6a4472f',1,'tatami::SparseRange']]]
 ];
