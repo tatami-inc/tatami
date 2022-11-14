27affdcc4252b2fdc798dc92bde2843217258b59
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2022-11-14 03:23:16 +0000

    Deploying to gh-pages from @ LTLA/tatami@0d0a531e13e550c795fb78645a95155fdfe4d519 🚀

--- docs/search/files_5.js
+++ docs/search/files_5.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['layeredmatrixdata_2ehpp_0',['LayeredMatrixData.hpp',['../LayeredMatrixData_8hpp.html',1,'']]]
+  ['layeredmatrixdata_2ehpp_0',['LayeredMatrixData.hpp',['../LayeredMatrixData_8hpp.html',1,'']]],
+  ['load_5fhdf5_5fmatrix_2ehpp_1',['load_hdf5_matrix.hpp',['../load__hdf5__matrix_8hpp.html',1,'']]]
 ];
--- docs/search/functions_8.js
+++ docs/search/functions_8.js
@@ -1,7 +1,9 @@
 var searchData=
 [
-  ['load_5flayered_5fsparse_5fmatrix_5ffrom_5fbuffer_0',['load_layered_sparse_matrix_from_buffer',['../MatrixMarket__layered_8hpp.html#af7655515516f20bf22da4311b7e8dcc5',1,'tatami::MatrixMarket']]],
-  ['load_5flayered_5fsparse_5fmatrix_5ffrom_5ffile_1',['load_layered_sparse_matrix_from_file',['../MatrixMarket__layered_8hpp.html#a1957591502ac286ad6b1bcae78ee75f9',1,'tatami::MatrixMarket']]],
-  ['load_5fsparse_5fmatrix_5ffrom_5fbuffer_2',['load_sparse_matrix_from_buffer',['../MatrixMarket_8hpp.html#abca83cf0a5a51b93f78991719c8bd772',1,'tatami::MatrixMarket']]],
-  ['load_5fsparse_5fmatrix_5ffrom_5ffile_3',['load_sparse_matrix_from_file',['../MatrixMarket_8hpp.html#acb8ab6a2dba7c9b9088245792d8dd4c5',1,'tatami::MatrixMarket']]]
+  ['load_5fhdf5_5fcompressed_5fsparse_5fmatrix_0',['load_hdf5_compressed_sparse_matrix',['../load__hdf5__matrix_8hpp.html#a8e62b7595d4b0d1d06cb203952007b28',1,'tatami']]],
+  ['load_5fhdf5_5fdense_5fmatrix_1',['load_hdf5_dense_matrix',['../load__hdf5__matrix_8hpp.html#ad13e199dab7f4ab7c4d3045a032f8cac',1,'tatami']]],
+  ['load_5flayered_5fsparse_5fmatrix_5ffrom_5fbuffer_2',['load_layered_sparse_matrix_from_buffer',['../MatrixMarket__layered_8hpp.html#af7655515516f20bf22da4311b7e8dcc5',1,'tatami::MatrixMarket']]],
+  ['load_5flayered_5fsparse_5fmatrix_5ffrom_5ffile_3',['load_layered_sparse_matrix_from_file',['../MatrixMarket__layered_8hpp.html#a1957591502ac286ad6b1bcae78ee75f9',1,'tatami::MatrixMarket']]],
+  ['load_5fsparse_5fmatrix_5ffrom_5fbuffer_4',['load_sparse_matrix_from_buffer',['../MatrixMarket_8hpp.html#abca83cf0a5a51b93f78991719c8bd772',1,'tatami::MatrixMarket']]],
+  ['load_5fsparse_5fmatrix_5ffrom_5ffile_5',['load_sparse_matrix_from_file',['../MatrixMarket_8hpp.html#acb8ab6a2dba7c9b9088245792d8dd4c5',1,'tatami::MatrixMarket']]]
 ];
