560412e2377fa49470b33e36b71c6e87564cfc62
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-01-04 20:36:32 +1100

    Extended tests to improve coverage of DelayedCast methods.

--- tests/src/base/DelayedCast.cpp
+++ tests/src/base/DelayedCast.cpp
@@ -19,18 +19,42 @@ protected:
     std::shared_ptr<tatami::NumericMatrix> dense, sparse;
     std::shared_ptr<tatami::Matrix<float, size_t> > fdense, fsparse;
     std::shared_ptr<tatami::NumericMatrix> fdense_ref, fsparse_ref;
+
+    std::shared_ptr<tatami::Matrix<float, int> > fsparse_value;
+    std::shared_ptr<tatami::Matrix<double, size_t> > sparse_index;
 protected:
     void SetUp() {
-        dense = std::shared_ptr<tatami::NumericMatrix>(new tatami::DenseRowMatrix<double>(sparse_nrow, sparse_ncol, sparse_matrix));
-        sparse = tatami::convert_to_sparse<false>(dense.get()); // column-major.
+        {
+            dense = std::shared_ptr<tatami::NumericMatrix>(new tatami::DenseRowMatrix<double>(sparse_nrow, sparse_ncol, sparse_matrix));
+            sparse = tatami::convert_to_sparse<false>(dense.get()); // column-major.
+        }
 
+        // Both the value and indices are changed in type.
         std::vector<float> fsparse_matrix(sparse_matrix.begin(), sparse_matrix.end());
-        fdense = std::shared_ptr<tatami::Matrix<float, size_t> >(new tatami::DenseRowMatrix<float, size_t>(sparse_nrow, sparse_ncol, fsparse_matrix));
-        fsparse = tatami::convert_to_sparse<false, float, size_t>(fdense.get()); // column-major.
+        {
+            fdense = std::shared_ptr<tatami::Matrix<float, size_t> >(new tatami::DenseRowMatrix<float, size_t>(sparse_nrow, sparse_ncol, fsparse_matrix));
+            fsparse = tatami::convert_to_sparse<false, float, size_t>(fdense.get()); // column-major.
+        }
 
-        std::vector<double> dsparse_matrix(fsparse_matrix.begin(), fsparse_matrix.end());
-        fdense_ref = std::shared_ptr<tatami::NumericMatrix>(new tatami::DenseRowMatrix<double>(sparse_nrow, sparse_ncol, dsparse_matrix));
-        fsparse_ref = tatami::convert_to_sparse<false>(fdense_ref.get()); // column-major.
+        // Reference with reduced precision, for comparison with double->float->double casts.
+        {
+            std::vector<double> dsparse_matrix(fsparse_matrix.begin(), fsparse_matrix.end());
+            fdense_ref = std::shared_ptr<tatami::NumericMatrix>(new tatami::DenseRowMatrix<double>(sparse_nrow, sparse_ncol, std::move(dsparse_matrix)));
+            fsparse_ref = tatami::convert_to_sparse<false>(fdense_ref.get()); // column-major.
+        }
+
+        // Only the value is changed in type.
+        {
+            auto refdense = std::shared_ptr<tatami::Matrix<float, int> >(new tatami::DenseRowMatrix<float, int>(sparse_nrow, sparse_ncol, fsparse_matrix));
+            fsparse_value = tatami::convert_to_sparse<false, float, int>(refdense.get()); 
+        }
+
+        // Only the index is changed in type.
+        {
+            auto redense = std::shared_ptr<tatami::Matrix<double, size_t> >(new tatami::DenseRowMatrix<double, size_t>(sparse_nrow, sparse_ncol, sparse_matrix));
+            sparse_index = tatami::convert_to_sparse<false, double, size_t>(redense.get()); 
+        }
+
         return;
     }
 };
@@ -44,6 +68,10 @@ TEST_P(DelayedCastFullAccess, Dense) {
     size_t JUMP = GetParam();
 
     auto cast_dense = tatami::make_DelayedCast<double, int>(dense);
+    EXPECT_EQ(cast_dense->sparse(), dense->sparse());
+    EXPECT_EQ(cast_dense->prefer_rows(), dense->prefer_rows());
+    EXPECT_EQ(cast_dense->dimension_preference(), dense->dimension_preference());
+
     test_simple_row_access(cast_dense.get(), dense.get(), true, JUMP);
     test_simple_column_access(cast_dense.get(), dense.get(), true, JUMP);
 
@@ -62,6 +90,14 @@ TEST_P(DelayedCastFullAccess, Sparse) {
     auto cast_fsparse = tatami::make_DelayedCast<double, int>(fsparse);
     test_simple_row_access(cast_fsparse.get(), fsparse_ref.get(), true, JUMP);
     test_simple_column_access(cast_fsparse.get(), fsparse_ref.get(), true, JUMP);
+
+    auto cast_fsparse_value = tatami::make_DelayedCast<double, int>(fsparse_value);
+    test_simple_row_access(cast_fsparse_value.get(), fsparse_ref.get(), true, JUMP);
+    test_simple_column_access(cast_fsparse_value.get(), fsparse_ref.get(), true, JUMP);
+
+    auto cast_sparse_index = tatami::make_DelayedCast<double, int>(sparse_index);
+    test_simple_row_access(cast_sparse_index.get(), sparse.get(), true, JUMP);
+    test_simple_column_access(cast_sparse_index.get(), sparse.get(), true, JUMP);
 }
 
 INSTANTIATE_TEST_CASE_P(
@@ -103,6 +139,14 @@ TEST_P(DelayedCastSlicedAccess, Sparse) {
     auto cast_fsparse = tatami::make_DelayedCast<double, int>(fsparse);
     test_sliced_row_access(cast_fsparse.get(), fsparse_ref.get(), true, JUMP, FIRST, LEN, SHIFT);
     test_sliced_column_access(cast_fsparse.get(), fsparse_ref.get(), true, JUMP, FIRST, LEN, SHIFT);
+
+    auto cast_fsparse_value = tatami::make_DelayedCast<double, int>(fsparse_value);
+    test_sliced_row_access(cast_fsparse_value.get(), fsparse_ref.get(), true, JUMP, FIRST, LEN, SHIFT);
+    test_sliced_column_access(cast_fsparse_value.get(), fsparse_ref.get(), true, JUMP, FIRST, LEN, SHIFT);
+
+    auto cast_sparse_index = tatami::make_DelayedCast<double, int>(sparse_index);
+    test_sliced_row_access(cast_sparse_index.get(), sparse.get(), true, JUMP, FIRST, LEN, SHIFT);
+    test_sliced_column_access(cast_sparse_index.get(), sparse.get(), true, JUMP, FIRST, LEN, SHIFT);
 }
 
 INSTANTIATE_TEST_CASE_P(
