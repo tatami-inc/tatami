34030b0cb5c9149f0bf8e9bc7393b64e37554127
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-03-23 10:36:30 -0700

    Cut down on code, allow templating of workspace dimension.

--- include/tatami/base/Workspace.hpp
+++ include/tatami/base/Workspace.hpp
@@ -15,27 +15,29 @@
 namespace tatami {
 
 /**
- * @brief Base row workspace class.
+ * @brief Base workspace class.
+ * @tparam ROW Whether to perform row-wise extraction.
  *
- * This provides a workspace for row-wise extraction of data from `Matrix` instances.
- * Instances of this class cannot be constructed; it is only provided as a base class for `Matrix::new_row_workspace()` methods to return appropriate objects.
+ * This provides a workspace for extraction of entire vectors from `Matrix` instances, either for each row (if `ROW = true`) or column (otherwise).
+ * Instances of this class cannot be constructed; it is only provided as a base class for `Matrix::new_row_workspace()` and `Matrix::new_column_workspace()` methods to return appropriate objects.
  */
-class RowWorkspace {
+template<bool ROW>
+class Workspace {
 protected:
     /**
      * @cond
      */
-    virtual ~RowWorkspace() = default;
+    virtual ~Workspace() = default;
 
     // Defining the other constructors for rule of 5. The move constructors
     // don't get auto-made when a destructor is declared, and if I'm defining
     // them, I might as well define the copy constructors.  Technically I
     // suppose I don't need them because this class is just an interface, but
     // who knows if I'll add some move-able stuff here later.
-    RowWorkspace(RowWorkspace&&) = default;
-    RowWorkspace& operator=(RowWorkspace&&) = default;
-    RowWorkspace(const RowWorkspace&) = default;
-    RowWorkspace& operator=(const RowWorkspace&) = default;
+    Workspace(Workspace&&) = default;
+    Workspace& operator=(Workspace&&) = default;
+    Workspace(const Workspace&) = default;
+    Workspace& operator=(const Workspace&) = default;
     /**
      * @endcond
      */
@@ -44,25 +46,39 @@ public:
     /**
      * Default constructor.
      */
-    RowWorkspace() = default;
+    Workspace() = default;
 };
 
 /**
- * @brief Base column workspace class.
- *
- * This provides a workspace for column-wise extraction of data from `Matrix` instances.
- * Instances of this class cannot be constructed; it is only provided as a base class for `Matrix::new_column_workspace()` methods to return appropriate objects.
+ * Base workspace class for extracting entire rows.
  */
-class ColumnWorkspace {
+typedef Workspace<true> RowWorkspace;
+
+/**
+ * Base workspace class for extracting entire columns.
+ */
+typedef Workspace<false> ColumnWorkspace;
+
+/**
+ * @brief Base workspace class for block extraction.
+ * @tparam ROW Whether to perform row-wise extraction.
+ *
+ * This provides a workspace for extraction of a block of data from `Matrix` instances.
+ * If `ROW = true`, each row-wise vector is extracted and the block is defined as a constant and contiguous set of columns from `[start, start + length)`.
+ * Otherwise, each column-wise vector is extracted and the block is defined as a constant and contiguous set of rows from `[start, start + length)`.
+ * Instances of this class cannot be constructed; it is only provided as a base class for `Matrix::new_row_workspace()` and `Matrix::new_column_workspace()` methods to return appropriate objects.
+ */
+template<bool ROW>
+class BlockWorkspace {
 protected:
     /**
      * @cond
      */
-    virtual ~ColumnWorkspace() = default;
-    ColumnWorkspace(ColumnWorkspace&&) = default;
-    ColumnWorkspace& operator=(ColumnWorkspace&&) = default;
-    ColumnWorkspace(const ColumnWorkspace&) = default;
-    ColumnWorkspace& operator=(const ColumnWorkspace&) = default;
+    virtual ~BlockWorkspace() = default;
+    BlockWorkspace(BlockWorkspace&&) = default;
+    BlockWorkspace& operator=(BlockWorkspace&&) = default;
+    BlockWorkspace(const BlockWorkspace&) = default;
+    BlockWorkspace& operator=(const BlockWorkspace&) = default;
     /**
      * @endcond
      */
@@ -71,116 +87,57 @@ public:
     /**
      * Default constructor.
      */
-    ColumnWorkspace() = default;
-};
-
-/**
- * @brief Base row-block workspace class.
- *
- * This provides a workspace for row-wise extraction of a block of data from `Matrix` instances,
- * where the block is defined as a constant and contiguous set of columns from `[start, end)`.
- * Instances of this class cannot be constructed; it is only provided as a base class for `Matrix::new_row_workspace()` methods to return appropriate objects.
- */
-class RowBlockWorkspace {
-protected:
-    /**
-     * @cond
-     */
-    virtual ~RowBlockWorkspace() = default;
-    RowBlockWorkspace(RowBlockWorkspace&&) = default;
-    RowBlockWorkspace& operator=(RowBlockWorkspace&&) = default;
-    RowBlockWorkspace(const RowBlockWorkspace&) = default;
-    RowBlockWorkspace& operator=(const RowBlockWorkspace&) = default;
-    /**
-     * @endcond
-     */
-
-public:
-    /**
-     * Default constructor.
-     */
-    RowBlockWorkspace() = default;
+    BlockWorkspace() = default;
 
     /**
-     * @param s First column in the block.
-     * @param l Number of columns in the block.
+     * @param s Index of the first row/column in the block.
+     * @param l Number of rows/columns in the block.
      */
-    RowBlockWorkspace(size_t s, size_t l) : start(s), length(l) {}
+    BlockWorkspace(size_t s, size_t l) : start(s), length(l) {}
 
     /**
-     * Index of the first column in the block.
+     * Index of the first row/column in the block.
      */
     size_t start;
 
     /**
-     * Number of columns in the block.
+     * Number of rows/columns in the block.
      */
     size_t length;
 };
 
 /**
- * @brief Base column-block workspace class.
- *
- * This provides a workspace for column-wise extraction of a block of data from `Matrix` instances,
- * where the block is defined as a constant and contiguous set of rows.
- * Instances of this class cannot be constructed; it is only provided as a base class for `Matrix::new_column_workspace()` methods to return appropriate objects.
+ * Base workspace class for extracting blocks of data from each row.
  */
-class ColumnBlockWorkspace {
-protected:
-    /**
-     * @cond
-     */
-    virtual ~ColumnBlockWorkspace() = default;
-    ColumnBlockWorkspace(ColumnBlockWorkspace&&) = default;
-    ColumnBlockWorkspace& operator=(ColumnBlockWorkspace&&) = default;
-    ColumnBlockWorkspace(const ColumnBlockWorkspace&) = default;
-    ColumnBlockWorkspace& operator=(const ColumnBlockWorkspace&) = default;
-    /**
-     * @endcond
-     */
-
-public:
-    /**
-     * Default constructor.
-     */
-    ColumnBlockWorkspace() = default;
-
-    /**
-     * @param s First row in the block.
-     * @param l Number of rows in the block.
-     */
-    ColumnBlockWorkspace(size_t s, size_t l) : start(s), length(l) {}
-
-    /**
-     * Index of the first row in the block.
-     */
-    size_t start;
-
-    /**
-     * Number of rows in the block.
-     */
-    size_t length;
-};
+typedef BlockWorkspace<true> RowBlockWorkspace;
 
 /**
+ * Base workspace class for extracting blocks of data from each column.
+ */
+typedef BlockWorkspace<false> ColumnBlockWorkspace;
+
+/**
+ * @brief Base workspace class for indexed extraction.
+ *
  * @tparam IDX Type of the column indices.
- * @brief Base row-index workspace class.
+ * @tparam ROW Whether to perform row-wise extraction.
  *
- * This provides a workspace for row-wise extraction of a subset of data from `Matrix` instances,
- * where the subset is defined from sorted and unique column indices.
- * Instances of this class cannot be constructed; it is only provided as a base class for `Matrix::new_row_workspace()` methods to return appropriate objects.
+ * This provides a workspace for extraction of a subset of data from `Matrix` instances.
+ * If `ROW = true`, each row-wise vector is extracted and the subset is defined from sorted and unique column indices.
+ * Otherwise, each column-wise vector is extracted and the subset is defined from sorted and unique row indices.
+ * Instances of this class cannot be constructed; it is only provided as a base class for `Matrix::new_row_workspace()` and `Matrix::new_column_workspace()` methods to return appropriate objects.
  */
-template<typename IDX>
-class RowIndexWorkspace {
+template<typename IDX, bool ROW>
+class IndexWorkspace {
 protected:
     /**
      * @cond
      */
-    virtual ~RowIndexWorkspace() = default;
-    RowIndexWorkspace(RowIndexWorkspace&&) = default;
-    RowIndexWorkspace& operator=(RowIndexWorkspace&&) = default;
-    RowIndexWorkspace(const RowIndexWorkspace&) = default;
-    RowIndexWorkspace& operator=(const RowIndexWorkspace&) = default;
+    virtual ~IndexWorkspace() = default;
+    IndexWorkspace(IndexWorkspace&&) = default;
+    IndexWorkspace& operator=(IndexWorkspace&&) = default;
+    IndexWorkspace(const IndexWorkspace&) = default;
+    IndexWorkspace& operator=(const IndexWorkspace&) = default;
     /**
      * @endcond
      */
@@ -189,73 +146,37 @@ public:
     /**
      * Default constructor.
      */
-    RowIndexWorkspace() = default;
+    IndexWorkspace() = default;
 
     /**
-     * @param l Number of columns in the subset.
-     * @param i Pointer to an array of sorted and unique column indices.
+     * @param l Number of rows/columns in the subset.
+     * @param i Pointer to an array of sorted and unique row/column indices.
      */
-    RowIndexWorkspace(size_t l, const IDX* i) : length(l), indices(i) {}
+    IndexWorkspace(size_t l, const IDX* i) : length(l), indices(i) {}
 
     /**
-     * Number of columns in the subset.
+     * Number of row/columns in the subset.
      */
     size_t length;
 
     /**
-     * Pointer to an array of length `length`, containing sorted and unique indices for columns in the subset.
+     * Pointer to an array of length `length`, containing sorted and unique indices for rows/columns in the subset.
      * It is assumed that the lifetime of the array exceeds that of this object.
      */
     const IDX* indices;
 };
 
 /**
- * @tparam IDX Type of the row indices.
- * @brief Base column-index workspace class.
- *
- * This provides a workspace for column-wise extraction of a subset of data from `Matrix` instances,
- * where the subset is defined from sorted and unique row indices.
- * Instances of this class cannot be constructed; it is only provided as a base class for `Matrix::new_column_workspace()` methods to return appropriate objects.
+ * Base workspace class for extracting subsets of data from each row.
  */
 template<typename IDX>
-class ColumnIndexWorkspace {
-protected:
-    /**
-     * @cond
-     */
-    virtual ~ColumnIndexWorkspace() = default;
-    ColumnIndexWorkspace(ColumnIndexWorkspace&&) = default;
-    ColumnIndexWorkspace& operator=(ColumnIndexWorkspace&&) = default;
-    ColumnIndexWorkspace(const ColumnIndexWorkspace&) = default;
-    ColumnIndexWorkspace& operator=(const ColumnIndexWorkspace&) = default;
-    /**
-     * @endcond
-     */
+using RowIndexWorkspace = IndexWorkspace<IDX, true>;
 
-public:
-    /**
-     * Default constructor.
-     */
-    ColumnIndexWorkspace() = default;
-
-    /**
-     * @param l Number of rows in the subset.
-     * @param i Pointer to an array of sorted and unique row indices.
-     */
-    ColumnIndexWorkspace(size_t l, const IDX* i) : length(l), indices(i) {}
-
-    /**
-     * Number of rows in the subset.
-     */
-    size_t length;
-
-    /**
-     * Pointer to an array of length `length`, containing sorted and unique indices for rows in the subset.
-     * It is assumed that the lifetime of the array exceeds that of this object.
-     */
-    const IDX* indices;
-};
-
+/**
+ * Base workspace class for extracting subsets of data from each column.
+ */
+template<typename IDX>
+using ColumnIndexWorkspace = IndexWorkspace<IDX, false>;
 
 
 }
