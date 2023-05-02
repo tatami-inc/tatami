b97ec5feb8f49bb16cc0efce6cf7a02c1b85fcfb
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-05-02 05:18:11 +0000

    Deploying to gh-pages from @ LTLA/tatami@98d6d5265906f735f96dab10cdd80080802eb00d 🚀

--- docs/search/classes_a.js
+++ docs/search/classes_a.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['options_0',['Options',['../structtatami_1_1Options.html',1,'tatami']]],
   ['oracle_1',['Oracle',['../structtatami_1_1Oracle.html',1,'tatami']]],
-  ['oracle_3c_20index_5fin_5f_20_3e_2',['Oracle&lt; Index_in_ &gt;',['../structtatami_1_1Oracle.html',1,'tatami']]]
+  ['oracle_3c_20index_5fin_5f_20_3e_2',['Oracle&lt; Index_in_ &gt;',['../structtatami_1_1Oracle.html',1,'tatami']]],
+  ['oraclestream_3',['OracleStream',['../structtatami_1_1OracleStream.html',1,'tatami']]]
 ];
