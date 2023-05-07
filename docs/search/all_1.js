84e2ee3191ae7f698388b7a614edbf7ccccde4ae
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-05-07 22:40:54 +0000

    Deploying to gh-pages from @ LTLA/tatami@7a3176c227273397ef7a4b75b7d3e8e08fbedd37 🚀

--- docs/search/all_1.js
+++ docs/search/all_1.js
@@ -1,12 +1,13 @@
 var searchData=
 [
-  ['begin_0',['begin',['../classtatami_1_1ArrayView.html#a476ff30f6f88958e34401d254a2ff77d',1,'tatami::ArrayView::begin()'],['../structtatami_1_1SomeNumericArray.html#a5b7999f71425029d4b4de32c2025fdd9',1,'tatami::SomeNumericArray::begin()']]],
-  ['bidimensionalapplyconfiguration_1',['BidimensionalApplyConfiguration',['../structtatami_1_1BidimensionalApplyConfiguration.html#ac0aa6d24276e347933038b432d4ce000',1,'tatami::BidimensionalApplyConfiguration::BidimensionalApplyConfiguration()'],['../structtatami_1_1BidimensionalApplyConfiguration.html',1,'tatami::BidimensionalApplyConfiguration&lt; row_, Value_, Index_ &gt;']]],
-  ['bind_5fintersection_2',['bind_intersection',['../namespacetatami.html#a87c52cf8a974ad0025550b002d391d39',1,'tatami']]],
-  ['bind_5fintersection_2ehpp_3',['bind_intersection.hpp',['../bind__intersection_8hpp.html',1,'']]],
-  ['block_5flength_4',['block_length',['../structtatami_1_1BlockExtractor.html#a2421eff5ead7d4b29306374de30ca8a2',1,'tatami::BlockExtractor']]],
-  ['block_5fstart_5',['block_start',['../structtatami_1_1BlockExtractor.html#a6c013beccbd49cfa2cf5aae42098a527',1,'tatami::BlockExtractor']]],
-  ['blockdenseextractor_6',['BlockDenseExtractor',['../namespacetatami.html#ae75de1fc78b7d361ea8b59a5379ea4da',1,'tatami']]],
-  ['blockextractor_7',['BlockExtractor',['../structtatami_1_1BlockExtractor.html',1,'tatami']]],
-  ['blocksparseextractor_8',['BlockSparseExtractor',['../namespacetatami.html#ac8d0024399a66ce61f6315f5f46ebb63',1,'tatami']]]
+  ['back_0',['back',['../structtatami_1_1OracleStream.html#a3ce855b3a8ffa7887b20f489faf1a68e',1,'tatami::OracleStream']]],
+  ['begin_1',['begin',['../classtatami_1_1ArrayView.html#a476ff30f6f88958e34401d254a2ff77d',1,'tatami::ArrayView::begin()'],['../structtatami_1_1SomeNumericArray.html#a5b7999f71425029d4b4de32c2025fdd9',1,'tatami::SomeNumericArray::begin()']]],
+  ['bidimensionalapplyconfiguration_2',['BidimensionalApplyConfiguration',['../structtatami_1_1BidimensionalApplyConfiguration.html#ac0aa6d24276e347933038b432d4ce000',1,'tatami::BidimensionalApplyConfiguration::BidimensionalApplyConfiguration()'],['../structtatami_1_1BidimensionalApplyConfiguration.html',1,'tatami::BidimensionalApplyConfiguration&lt; row_, Value_, Index_ &gt;']]],
+  ['bind_5fintersection_3',['bind_intersection',['../namespacetatami.html#a87c52cf8a974ad0025550b002d391d39',1,'tatami']]],
+  ['bind_5fintersection_2ehpp_4',['bind_intersection.hpp',['../bind__intersection_8hpp.html',1,'']]],
+  ['block_5flength_5',['block_length',['../structtatami_1_1BlockExtractor.html#a2421eff5ead7d4b29306374de30ca8a2',1,'tatami::BlockExtractor']]],
+  ['block_5fstart_6',['block_start',['../structtatami_1_1BlockExtractor.html#a6c013beccbd49cfa2cf5aae42098a527',1,'tatami::BlockExtractor']]],
+  ['blockdenseextractor_7',['BlockDenseExtractor',['../namespacetatami.html#ae75de1fc78b7d361ea8b59a5379ea4da',1,'tatami']]],
+  ['blockextractor_8',['BlockExtractor',['../structtatami_1_1BlockExtractor.html',1,'tatami']]],
+  ['blocksparseextractor_9',['BlockSparseExtractor',['../namespacetatami.html#ac8d0024399a66ce61f6315f5f46ebb63',1,'tatami']]]
 ];
--- docs/search/functions_1.js
+++ docs/search/functions_1.js
@@ -1,6 +1,7 @@
 var searchData=
 [
-  ['begin_0',['begin',['../classtatami_1_1ArrayView.html#a476ff30f6f88958e34401d254a2ff77d',1,'tatami::ArrayView::begin()'],['../structtatami_1_1SomeNumericArray.html#a5b7999f71425029d4b4de32c2025fdd9',1,'tatami::SomeNumericArray::begin()']]],
-  ['bidimensionalapplyconfiguration_1',['BidimensionalApplyConfiguration',['../structtatami_1_1BidimensionalApplyConfiguration.html#ac0aa6d24276e347933038b432d4ce000',1,'tatami::BidimensionalApplyConfiguration']]],
-  ['bind_5fintersection_2',['bind_intersection',['../namespacetatami.html#a87c52cf8a974ad0025550b002d391d39',1,'tatami']]]
+  ['back_0',['back',['../structtatami_1_1OracleStream.html#a3ce855b3a8ffa7887b20f489faf1a68e',1,'tatami::OracleStream']]],
+  ['begin_1',['begin',['../classtatami_1_1ArrayView.html#a476ff30f6f88958e34401d254a2ff77d',1,'tatami::ArrayView::begin()'],['../structtatami_1_1SomeNumericArray.html#a5b7999f71425029d4b4de32c2025fdd9',1,'tatami::SomeNumericArray::begin()']]],
+  ['bidimensionalapplyconfiguration_2',['BidimensionalApplyConfiguration',['../structtatami_1_1BidimensionalApplyConfiguration.html#ac0aa6d24276e347933038b432d4ce000',1,'tatami::BidimensionalApplyConfiguration']]],
+  ['bind_5fintersection_3',['bind_intersection',['../namespacetatami.html#a87c52cf8a974ad0025550b002d391d39',1,'tatami']]]
 ];
