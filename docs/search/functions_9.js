7af6fd4c2adf7af99f5487874fa3c453211206c9
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-05-16 23:29:06 +0000

    Deploying to gh-pages from @ tatami-inc/tatami@ee833d16e0ce97a07624e46239aa196b12f6e681 🚀

--- docs/search/functions_9.js
+++ docs/search/functions_9.js
@@ -1,13 +1,13 @@
 var searchData=
 [
   ['make_5fdelayedaddvectorhelper_0',['make_DelayedAddVectorHelper',['../namespacetatami.html#aa82419090e54a4674d971429e93e6af5',1,'tatami']]],
-  ['make_5fdelayedbind_1',['make_DelayedBind',['../namespacetatami.html#a8b867f096bc0997f4fafb2e6d6a487f0',1,'tatami']]],
-  ['make_5fdelayedcast_2',['make_DelayedCast',['../namespacetatami.html#a87de66c4343d2a873437de6b89e20185',1,'tatami']]],
+  ['make_5fdelayedbind_1',['make_DelayedBind',['../namespacetatami.html#a5f8e0f69139575707aa9314174b415b3',1,'tatami']]],
+  ['make_5fdelayedcast_2',['make_DelayedCast',['../namespacetatami.html#aca62c3bf751cdd06a08e8e503b0b591a',1,'tatami']]],
   ['make_5fdelayeddividevectorhelper_3',['make_DelayedDivideVectorHelper',['../namespacetatami.html#ab104e5029db4d09afe0b47a125f457bf',1,'tatami']]],
-  ['make_5fdelayedisometricop_4',['make_DelayedIsometricOp',['../namespacetatami.html#afc6db928df0451c6794676e3c9e11960',1,'tatami']]],
+  ['make_5fdelayedisometricop_4',['make_DelayedIsometricOp',['../namespacetatami.html#a70e92d6c4eb0b1303b02176aa1d016a0',1,'tatami']]],
   ['make_5fdelayedmultiplyvectorhelper_5',['make_DelayedMultiplyVectorHelper',['../namespacetatami.html#a316f4cf0ae5a9e83c2dad6ccbbbcfb43',1,'tatami']]],
-  ['make_5fdelayedsubset_6',['make_DelayedSubset',['../namespacetatami.html#a09fc71e54860b6905d05c596612af35e',1,'tatami']]],
-  ['make_5fdelayedsubsetblock_7',['make_DelayedSubsetBlock',['../namespacetatami.html#a1f41ac7fe0c90c099fe08d1afc489ed9',1,'tatami']]],
+  ['make_5fdelayedsubset_6',['make_DelayedSubset',['../namespacetatami.html#abfc63177b00e6e3e2fa47754b8d87704',1,'tatami']]],
+  ['make_5fdelayedsubsetblock_7',['make_DelayedSubsetBlock',['../namespacetatami.html#a8f4aafc0a1fbdc0c31bc122d24122a63',1,'tatami']]],
   ['make_5fdelayedsubtractvectorhelper_8',['make_DelayedSubtractVectorHelper',['../namespacetatami.html#abb68a8d27e821f213965edab7f029c48',1,'tatami']]],
-  ['make_5fdelayedtranspose_9',['make_DelayedTranspose',['../namespacetatami.html#acbbfeb0c2d1cbc9db6d2d023994fe524',1,'tatami']]]
+  ['make_5fdelayedtranspose_9',['make_DelayedTranspose',['../namespacetatami.html#afa35d8e9fe286967f327ec0eb6bd5005',1,'tatami']]]
 ];
