772b2d97af60212b14672713a3b9d753e2947283
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2023-07-29 00:50:28 -0700

    Incorporated default <threads> parallelization when OpenMP isn't available.
    Overhauled the tests and actions to check OpenMP all the time (if available).

--- include/tatami/stats/utils.hpp
+++ include/tatami/stats/utils.hpp
@@ -7,6 +7,15 @@
 
 #include <cmath>
 
+#ifndef TATAMI_CUSTOM_PARALLEL
+#ifndef _OPENMP
+#include <thread>
+#endif
+#include <string>
+#include <stdexcept>
+#include <vector>
+#endif
+
 /**
  * @file utils.hpp
  *
@@ -16,9 +25,13 @@
 namespace tatami {
 
 /**
- * Apply a function to a set of tasks, distributing them to threads via OpenMP if enabled.
- * Callers can specify a custom parallelization scheme by defining a `TATAMI_CUSTOM_PARALLEL` function-like macro, 
- * which should accept the `fun`, `tasks` and `threads` arguments as below.
+ * Apply a function to a set of tasks in parallel.
+ * This can be done using:
+ *
+ * - OpenMP, if available and enabled by the compiler.
+ * - Using a custom parallelization scheme, by defining a `TATAMI_CUSTOM_PARALLEL` function-like macro. 
+ *   This should accept the `fun`, `tasks` and `threads` arguments as below.
+ * - `<thread>`, otherwise.
  *
  * @tparam parallel_ Whether the tasks should be run in parallel.
  * If `false`, no parallelization is performed and all tasks are run on the current thread.
@@ -37,33 +50,66 @@ namespace tatami {
  * @param threads Number of threads.
  */
 template<bool parallel_ = true, class Function_, typename Index_>
-void parallelize(Function_ fun, Index_ tasks, size_t
-#if defined(_OPENMP) || defined(TATAMI_CUSTOM_PARALLEL)
-    threads // wrap here to avoid used variable warnings (which are in turn converted to errors).
-#endif
-) {
-#if defined(_OPENMP) || defined(TATAMI_CUSTOM_PARALLEL)
-
+void parallelize(Function_ fun, Index_ tasks, size_t threads) {
     if constexpr(parallel_) {
-
         if (threads > 1) {
 #ifndef TATAMI_CUSTOM_PARALLEL
             Index_ worker_size = (tasks / threads) + (tasks % threads > 0); // Ceiling of an integer division.
             threads = (tasks / worker_size) + (tasks % worker_size > 0); // Set the actual number of required threads.
+            std::vector<std::string> errors(threads);
 
+#if defined(_OPENMP)
             #pragma omp parallel for num_threads(threads)
             for (size_t t = 0; t < threads; ++t) {
                 Index_ start = worker_size * t; // Will not overflow due to the above recomputation of 'threads'.
                 Index_ remaining = tasks - start; // Must be positive, as otherwise 'tasks % worker_size = 0' and the iteration wouldn't even get here.
-                fun(t, start, std::min(remaining, worker_size)); // Use 'remaining' to avoid potential overflow from computing 'end = start + worker_size'.
+
+                try {
+                    fun(t, start, std::min(remaining, worker_size)); // Use 'remaining' to avoid potential overflow from computing 'end = start + worker_size'.
+                } catch (std::exception& e) {
+                    errors[t] = e.what();
+                } catch (...) {
+                    errors[t] = "unknown error in thread " + std::to_string(t);
+                }
             }
+
+#else
+            Index_ first = 0;
+            std::vector<std::thread> workers;
+            workers.reserve(threads);
+
+            for (size_t t = 0; t < threads && first < tasks; ++t) {
+                Index_ remaining = tasks - first;
+                Index_ len = std::min(remaining, worker_size);
+                workers.emplace_back([&fun,&errors](int t, Index_ first, Index_ len) -> void {
+                    try {
+                        fun(t, first, len);
+                    } catch (std::exception& e) {
+                        errors[t] = e.what();
+                    } catch (...) {
+                        errors[t] = "unknown error in thread " + std::to_string(t);
+                    }
+                }, t, first, len);
+                first += len;
+            }
+
+            for (auto& wrk : workers) {
+                wrk.join();
+            }
+#endif
+
+            for (const auto& e : errors) {
+                if (!e.empty()) {
+                    throw std::runtime_error(e);
+                }
+            }
+
 #else
             TATAMI_CUSTOM_PARALLEL(std::move(fun), tasks, threads);
 #endif
             return;
         }
     }
-#endif
 
     fun(0, 0, tasks);
     return;
