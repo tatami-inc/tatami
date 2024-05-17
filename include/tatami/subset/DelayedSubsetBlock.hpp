122339e879177a008500423785ec8399750a8dd2
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-05-16 17:21:25 -0700

    Doesnt quite work but i need to have dinner now.

--- include/tatami/subset/DelayedSubsetBlock.hpp
+++ include/tatami/subset/DelayedSubsetBlock.hpp
@@ -35,114 +35,116 @@ void bump_indices(VectorPtr<Index_>& indices_ptr, Index_ subset_start) {
 }
 
 template<bool oracle_, typename Value_, typename Index_>
-struct AlongDense : public DenseExtractor<oracle_, Value_, Index_> {
-    AlongDense(const Matrix<Value_, Index_>* mat, Index_ subset_start, Index_ subset_length, bool row, MaybeOracle<oracle_, Index_> oracle, const Options& opt) :
-        internal(new_extractor<false, oracle_>(mat, row, std::move(oracle), subset_start, subset_length, opt)) {}
+class AlongDense : public DenseExtractor<oracle_, Value_, Index_> {
+public:
+    AlongDense(const Matrix<Value_, Index_>* matrix, Index_ subset_start, Index_ subset_length, bool row, MaybeOracle<oracle_, Index_> oracle, const Options& opt) :
+        my_ext(new_extractor<false, oracle_>(matrix, row, std::move(oracle), subset_start, subset_length, opt)) {}
 
-    AlongDense(const Matrix<Value_, Index_>* mat, Index_ subset_start, [[maybe_unused]] Index_ subset_length, bool row, MaybeOracle<oracle_, Index_> oracle, Index_ block_start, Index_ block_length, const Options& opt) :
-        internal(new_extractor<false, oracle_>(mat, row, std::move(oracle), subset_start + block_start, block_length, opt)) {}
+    AlongDense(const Matrix<Value_, Index_>* matrix, Index_ subset_start, Index_ /* for consistency */, bool row, MaybeOracle<oracle_, Index_> oracle, Index_ block_start, Index_ block_length, const Options& opt) :
+        my_ext(new_extractor<false, oracle_>(matrix, row, std::move(oracle), subset_start + block_start, block_length, opt)) {}
 
-    AlongDense(const Matrix<Value_, Index_>* mat, Index_ subset_start, [[maybe_unused]] Index_ subset_length, bool row, MaybeOracle<oracle_, Index_> oracle, VectorPtr<Index_> indices_ptr, const Options& opt) {
+    AlongDense(const Matrix<Value_, Index_>* matrix, Index_ subset_start, Index_ /* for consistency */, bool row, MaybeOracle<oracle_, Index_> oracle, VectorPtr<Index_> indices_ptr, const Options& opt) {
         bump_indices(indices_ptr, subset_start); 
-        internal = new_extractor<false, oracle_>(mat, row, std::move(oracle), std::move(indices_ptr), opt);
+        my_ext = new_extractor<false, oracle_>(matrix, row, std::move(oracle), std::move(indices_ptr), opt);
     }
 
-public:
     const Value_* fetch(Index_ i, Value_* buffer) {
-        return internal->fetch(i, buffer);
+        return my_ext->fetch(i, buffer);
     }
 
 private:
-    std::unique_ptr<DenseExtractor<oracle_, Value_, Index_> > internal;
+    std::unique_ptr<DenseExtractor<oracle_, Value_, Index_> > my_ext;
 };
 
 template<bool oracle_, typename Value_, typename Index_>
-struct AlongSparse : public SparseExtractor<oracle_, Value_, Index_> {
-    AlongSparse(const Matrix<Value_, Index_>* mat, Index_ subset_start, Index_ subset_length, bool row, MaybeOracle<oracle_, Index_> oracle, const Options& opt) :
-        internal(new_extractor<true, oracle_>(mat, row, std::move(oracle), subset_start, subset_length, opt)), shift(subset_start) {}
+class AlongSparse : public SparseExtractor<oracle_, Value_, Index_> {
+public:
+    AlongSparse(const Matrix<Value_, Index_>* matrix, Index_ subset_start, Index_ subset_length, bool row, MaybeOracle<oracle_, Index_> oracle, const Options& opt) :
+        my_ext(new_extractor<true, oracle_>(matrix, row, std::move(oracle), subset_start, subset_length, opt)), my_shift(subset_start) {}
 
-    AlongSparse(const Matrix<Value_, Index_>* mat, Index_ subset_start, [[maybe_unused]] Index_ subset_length, bool row, MaybeOracle<oracle_, Index_> oracle, Index_ block_start, Index_ block_length, const Options& opt) :
-        internal(new_extractor<true, oracle_>(mat, row, std::move(oracle), subset_start + block_start, block_length, opt)), shift(subset_start) {}
+    AlongSparse(const Matrix<Value_, Index_>* matrix, Index_ subset_start, Index_ /* for consistency */, bool row, MaybeOracle<oracle_, Index_> oracle, Index_ block_start, Index_ block_length, const Options& opt) :
+        my_ext(new_extractor<true, oracle_>(matrix, row, std::move(oracle), subset_start + block_start, block_length, opt)), my_shift(subset_start) {}
 
-    AlongSparse(const Matrix<Value_, Index_>* mat, Index_ subset_start, [[maybe_unused]] Index_ subset_length, bool row, MaybeOracle<oracle_, Index_> oracle, VectorPtr<Index_> indices_ptr, const Options& opt) : 
-        shift(subset_start) 
+    AlongSparse(const Matrix<Value_, Index_>* matrix, Index_ subset_start, Index_ /* for consistency */, bool row, MaybeOracle<oracle_, Index_> oracle, VectorPtr<Index_> indices_ptr, const Options& opt) : 
+        my_shift(subset_start) 
     {
         bump_indices(indices_ptr, subset_start); 
-        internal = new_extractor<true, oracle_>(mat, row, std::move(oracle), std::move(indices_ptr), opt);
+        my_ext = new_extractor<true, oracle_>(matrix, row, std::move(oracle), std::move(indices_ptr), opt);
     }
 
-public:
-    SparseRange<Value_, Index_> fetch(Index_ i, Value_* vbuffer, Index_* ibuffer) {
-        auto output = internal->fetch(i, vbuffer, ibuffer);
-        if (output.index && shift) {
+    SparseRange<Value_, Index_> fetch(Index_ i, Value_* value_buffer, Index_* index_buffer) {
+        auto output = my_ext->fetch(i, value_buffer, index_buffer);
+        if (output.index && my_shift) {
             for (Index_ i = 0; i < output.number; ++i) {
-                ibuffer[i] = output.index[i] - shift;
+                index_buffer[i] = output.index[i] - my_shift;
             }
-            output.index = ibuffer;
+            output.index = index_buffer;
         }
         return output;
     }
 
 private:
-    std::unique_ptr<SparseExtractor<oracle_, Value_, Index_> > internal;
-    Index_ shift;
+    std::unique_ptr<SparseExtractor<oracle_, Value_, Index_> > my_ext;
+    Index_ my_shift;
 };
 
 template<typename Index_>
-struct SubsetOracle : public Oracle<Index_> {
-    SubsetOracle(std::shared_ptr<const Oracle<Index_> > input, Index_ shift) : input(std::move(input)), shift(shift) {}
+class SubsetOracle : public Oracle<Index_> {
+public:
+    SubsetOracle(std::shared_ptr<const Oracle<Index_> > oracle, Index_ shift) : my_oracle(std::move(oracle)), my_shift(shift) {}
 
     size_t total() const {
-        return input->total();
+        return my_oracle->total();
     }
 
     Index_ get(size_t i) const {
-        return input->get(i) + shift;
+        return my_oracle->get(i) + my_shift;
     }
 
 private:
-    std::shared_ptr<const Oracle<Index_> > input;
-    Index_ shift;
+    std::shared_ptr<const Oracle<Index_> > my_oracle;
+    Index_ my_shift;
 };
 
 template<bool oracle_, typename Value_, typename Index_>
-struct AcrossDense : public DenseExtractor<oracle_, Value_, Index_> {
+class AcrossDense : public DenseExtractor<oracle_, Value_, Index_> {
+public:
     template<typename ... Args_>
-    AcrossDense(const Matrix<Value_, Index_>* mat, Index_ subset_start, bool row, MaybeOracle<oracle_, Index_> oracle, Args_&& ... args) : shift(subset_start) {
+    AcrossDense(const Matrix<Value_, Index_>* matrix, Index_ subset_start, bool row, MaybeOracle<oracle_, Index_> oracle, Args_&& ... args) : my_shift(subset_start) {
         if constexpr(oracle_) {
-            auto ptr = new SubsetOracle(std::move(oracle), shift);
+            auto ptr = new SubsetOracle(std::move(oracle), my_shift);
             oracle.reset(ptr);
         } 
-        internal = new_extractor<false, oracle_>(mat, row, std::move(oracle), std::forward<Args_>(args)...);
+        my_ext = new_extractor<false, oracle_>(matrix, row, std::move(oracle), std::forward<Args_>(args)...);
     }
 
     const Value_* fetch(Index_ i, Value_* buffer) {
-        return internal->fetch(i + shift, buffer);
+        return my_ext->fetch(i + my_shift, buffer);
     }
 
 private:
-    std::unique_ptr<DenseExtractor<oracle_, Value_, Index_> > internal;
-    Index_ shift;
+    std::unique_ptr<DenseExtractor<oracle_, Value_, Index_> > my_ext;
+    Index_ my_shift;
 };
 
 template<bool oracle_, typename Value_, typename Index_>
 struct AcrossSparse : public SparseExtractor<oracle_, Value_, Index_> {
     template<typename ... Args_>
-    AcrossSparse(const Matrix<Value_, Index_>* mat, Index_ subset_start, bool row, MaybeOracle<oracle_, Index_> oracle, Args_&& ... args) : shift(subset_start) {
+    AcrossSparse(const Matrix<Value_, Index_>* matrix, Index_ subset_start, bool row, MaybeOracle<oracle_, Index_> oracle, Args_&& ... args) : my_shift(subset_start) {
         if constexpr(oracle_) {
-            auto ptr = new SubsetOracle(std::move(oracle), shift);
+            auto ptr = new SubsetOracle(std::move(oracle), my_shift);
             oracle.reset(ptr);
         }
-        internal = new_extractor<true, oracle_>(mat, row, std::move(oracle), std::forward<Args_>(args)...);
+        my_ext = new_extractor<true, oracle_>(matrix, row, std::move(oracle), std::forward<Args_>(args)...);
     }
 
-    SparseRange<Value_, Index_> fetch(Index_ i, Value_* vbuffer, Index_* ibuffer) {
-        return internal->fetch(i + shift, vbuffer, ibuffer);
+    SparseRange<Value_, Index_> fetch(Index_ i, Value_* value_buffer, Index_* index_buffer) {
+        return my_ext->fetch(i + my_shift, value_buffer, index_buffer);
     }
 
 private:
-    std::unique_ptr<SparseExtractor<oracle_, Value_, Index_> > internal;
-    Index_ shift;
+    std::unique_ptr<SparseExtractor<oracle_, Value_, Index_> > my_ext;
+    Index_ my_shift;
 };
 
 }
@@ -164,55 +166,55 @@ template<typename Value_, typename Index_>
 class DelayedSubsetBlock : public Matrix<Value_, Index_> {
 public:
     /**
-     * @param p Pointer to the underlying (pre-subset) matrix.
-     * @param s Index of the start of the block. This should be a row index if `row = true` and a column index otherwise.
-     * @param l Length of the block, in terms of the number of rows (if `row = true`) or columns (otherwise).
-     * @param r Whether to apply the subset to the rows.
+     * @param matrix Pointer to the underlying (pre-subset) matrix.
+     * @param subset_start Index of the start of the block. This should be a row index if `row = true` and a column index otherwise.
+     * @param subset_length Length of the block, in terms of the number of rows (if `row = true`) or columns (otherwise).
+     * @param row Whether to apply the subset to the rows.
      * If false, the subset is applied to the columns.
      */
-    DelayedSubsetBlock(std::shared_ptr<const Matrix<Value_, Index_> > p, Index_ s, Index_ l, bool r) : 
-        mat(std::move(p)), block_start(s), block_length(l), by_row(r) {}
+    DelayedSubsetBlock(std::shared_ptr<const Matrix<Value_, Index_> > matrix, Index_ subset_start, Index_ subset_length, bool row) : 
+        my_matrix(std::move(matrix)), my_subset_start(subset_start), my_subset_length(subset_length), my_row(row) {}
 
 private:
-    std::shared_ptr<const Matrix<Value_, Index_> > mat;
-    Index_ block_start, block_length;
-    bool by_row;
+    std::shared_ptr<const Matrix<Value_, Index_> > my_matrix;
+    Index_ my_subset_start, my_subset_length;
+    bool my_row;
 
 public:
     Index_ nrow() const {
-        if (by_row) {
-            return block_length;
+        if (my_row) {
+            return my_subset_length;
         } else {
-            return mat->nrow();
+            return my_matrix->nrow();
         }
     }
 
     Index_ ncol() const {
-        if (by_row) {
-            return mat->ncol();
+        if (my_row) {
+            return my_matrix->ncol();
         } else {
-            return block_length;
+            return my_subset_length;
         }
     }
 
     bool is_sparse() const {
-        return mat->is_sparse();
+        return my_matrix->is_sparse();
     }
 
     double is_sparse_proportion() const {
-        return mat->is_sparse_proportion();
+        return my_matrix->is_sparse_proportion();
     }
 
     bool prefer_rows() const {
-        return mat->prefer_rows();
+        return my_matrix->prefer_rows();
     }
 
     double prefer_rows_proportion() const {
-        return mat->prefer_rows_proportion();
+        return my_matrix->prefer_rows_proportion();
     }
 
     bool uses_oracle(bool row) const {
-        return mat->uses_oracle(row);
+        return my_matrix->uses_oracle(row);
     }
 
     using Matrix<Value_, Index_>::dense_column;
@@ -229,10 +231,10 @@ public:
 private:
     template<bool oracle_, typename ... Args_>
     std::unique_ptr<DenseExtractor<oracle_, Value_, Index_> > dense_internal(bool row, Args_&&... args) const {
-        if (row != by_row) {
-            return std::make_unique<DelayedSubsetBlock_internal::AlongDense<oracle_, Value_, Index_> >(mat.get(), block_start, block_length, row, std::forward<Args_>(args)...);
+        if (row != my_row) {
+            return std::make_unique<DelayedSubsetBlock_internal::AlongDense<oracle_, Value_, Index_> >(my_matrix.get(), my_subset_start, my_subset_length, row, std::forward<Args_>(args)...);
         } else {
-            return std::make_unique<DelayedSubsetBlock_internal::AcrossDense<oracle_, Value_, Index_> >(mat.get(), block_start, row, std::forward<Args_>(args)...);
+            return std::make_unique<DelayedSubsetBlock_internal::AcrossDense<oracle_, Value_, Index_> >(my_matrix.get(), my_subset_start, row, std::forward<Args_>(args)...);
         }
     }
 
@@ -255,10 +257,10 @@ public:
 private:
     template<bool oracle_, typename ... Args_>
     std::unique_ptr<SparseExtractor<oracle_, Value_, Index_> > sparse_internal(bool row, Args_&&... args) const {
-        if (row != by_row) {
-            return std::make_unique<DelayedSubsetBlock_internal::AlongSparse<oracle_, Value_, Index_> >(mat.get(), block_start, block_length, row, std::forward<Args_>(args)...);
+        if (row != my_row) {
+            return std::make_unique<DelayedSubsetBlock_internal::AlongSparse<oracle_, Value_, Index_> >(my_matrix.get(), my_subset_start, my_subset_length, row, std::forward<Args_>(args)...);
         } else {
-            return std::make_unique<DelayedSubsetBlock_internal::AcrossSparse<oracle_, Value_, Index_> >(mat.get(), block_start, row, std::forward<Args_>(args)...);
+            return std::make_unique<DelayedSubsetBlock_internal::AcrossSparse<oracle_, Value_, Index_> >(my_matrix.get(), my_subset_start, row, std::forward<Args_>(args)...);
         }
     }
 
@@ -271,8 +273,8 @@ public:
         return sparse_internal<false>(row, false, block_start, block_length, opt);
     }
 
-    std::unique_ptr<MyopicSparseExtractor<Value_, Index_> > sparse(bool row, VectorPtr<Index_> indices, const Options& opt) const {
-        return sparse_internal<false>(row, false, std::move(indices), opt);
+    std::unique_ptr<MyopicSparseExtractor<Value_, Index_> > sparse(bool row, VectorPtr<Index_> indices_ptr, const Options& opt) const {
+        return sparse_internal<false>(row, false, std::move(indices_ptr), opt);
     }
 
     /**************************
@@ -314,25 +316,25 @@ public:
  * @tparam Value_ Type of matrix value.
  * @tparam Index_ Integer type for the row/column indices.
  *
- * @param p Pointer to the underlying (pre-subset) `Matrix`.
- * @param f Index of the start of the block. This should be a row index if `r = true` and a column index otherwise.
- * @param l Index of the one-past-the-end of the block.
- * @param r Whether to apply the subset to the rows.
+ * @param matrix Pointer to the underlying (pre-subset) `Matrix`.
+ * @param subset_start Index of the start of the block. This should be a row index if `r = true` and a column index otherwise.
+ * @param subset_length Index of the one-past-the-end of the block.
+ * @param row Whether to apply the subset to the rows.
  * If false, the subset is applied to the columns.
  *
  * @return A pointer to a `DelayedSubsetBlock` instance.
  */
 template<typename Value_, typename Index_>
-std::shared_ptr<Matrix<Value_, Index_> > make_DelayedSubsetBlock(std::shared_ptr<const Matrix<Value_, Index_> > p, Index_ f, Index_ l, bool r) {
-    return std::shared_ptr<Matrix<Value_, Index_> >(new DelayedSubsetBlock<Value_, Index_>(std::move(p), f, l, r));
+std::shared_ptr<Matrix<Value_, Index_> > make_DelayedSubsetBlock(std::shared_ptr<const Matrix<Value_, Index_> > matrix, Index_ subset_start, Index_ subset_length, bool row) {
+    return std::shared_ptr<Matrix<Value_, Index_> >(new DelayedSubsetBlock<Value_, Index_>(std::move(matrix), subset_start, subset_length, row));
 }
 
 /**
  * @cond
  */
 template<typename Value_, typename Index_>
-std::shared_ptr<Matrix<Value_, Index_> > make_DelayedSubsetBlock(std::shared_ptr<Matrix<Value_, Index_> > p, Index_ f, Index_ l, bool r) {
-    return std::shared_ptr<Matrix<Value_, Index_> >(new DelayedSubsetBlock<Value_, Index_>(std::move(p), f, l, r));
+std::shared_ptr<Matrix<Value_, Index_> > make_DelayedSubsetBlock(std::shared_ptr<Matrix<Value_, Index_> > matrix, Index_ subset_start, Index_ subset_length, bool row) {
+    return std::shared_ptr<Matrix<Value_, Index_> >(new DelayedSubsetBlock<Value_, Index_>(std::move(matrix), subset_start, subset_length, row));
 }
 /**
  * @endcond
@@ -342,13 +344,13 @@ std::shared_ptr<Matrix<Value_, Index_> > make_DelayedSubsetBlock(std::shared_ptr
  * @cond
  */
 template<int margin_, typename Value_, typename Index_>
-std::shared_ptr<Matrix<Value_, Index_> > make_DelayedSubsetBlock(std::shared_ptr<const Matrix<Value_, Index_> > p, Index_ f, Index_ l) {
-    return make_DelayedSubsetBlock(std::move(p), f, l, margin_ == 0);
+std::shared_ptr<Matrix<Value_, Index_> > make_DelayedSubsetBlock(std::shared_ptr<const Matrix<Value_, Index_> > matrix, Index_ subset_start, Index_ subset_length) {
+    return make_DelayedSubsetBlock(std::move(matrix), subset_start, subset_length, margin_ == 0);
 }
 
 template<int margin_, typename Value_, typename Index_>
-std::shared_ptr<Matrix<Value_, Index_> > make_DelayedSubsetBlock(std::shared_ptr<Matrix<Value_, Index_> > p, Index_ f, Index_ l) {
-    return make_DelayedSubsetBlock(std::move(p), f, l, margin_ == 0);
+std::shared_ptr<Matrix<Value_, Index_> > make_DelayedSubsetBlock(std::shared_ptr<Matrix<Value_, Index_> > matrix, Index_ subset_start, Index_ subset_length) {
+    return make_DelayedSubsetBlock(std::move(matrix), subset_start, subset_length, margin_ == 0);
 }
 /**
  * @endcond
