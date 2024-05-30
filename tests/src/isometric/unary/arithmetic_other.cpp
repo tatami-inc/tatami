c917fb717055091c9151a85defc331dff08622f3
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-05-30 08:56:30 -0700

    Test fixes.

--- tests/src/isometric/unary/arithmetic_other.cpp
+++ tests/src/isometric/unary/arithmetic_other.cpp
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "tatami/isometric/unary/arithmetic_helpers.hpp"
+#include "tatami_test/tatami_test.hpp"
 
 #include <limits>
 #include <vector>
@@ -53,38 +54,47 @@ TEST(DelayedUnaryIsometricArithmetic, Sparsity) {
 
 TEST(DelayedUnaryIsometricArithmetic, FillInt) {
     // Checking fill calculations for non-IEEE types.
+    auto res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::ADD, true, int>(2);
+    EXPECT_EQ(res, 2);
+
+    res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::MULTIPLY, true, int>(2);
+    EXPECT_EQ(res, 0);
+
+    res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::POWER, false, int>(2);
+    EXPECT_EQ(res, 1);
+
+    res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::DIVIDE, true, int>(2);
+    EXPECT_EQ(res, 0);
+
+    res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::INTEGER_DIVIDE, true, int>(2);
+    EXPECT_EQ(res, 0);
+
+    tatami_test::throws_error([&]() { tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::DIVIDE, true, int>(0); }, "division by zero");
+    tatami_test::throws_error([&]() { tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::DIVIDE, false, int>(2); }, "division by zero");
+}
+
+TEST(DelayedUnaryIsometricArithmetic, NonIeee754Ops) {
+    // Checking that it works inside helpers.
     {
-        auto res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::ADD, true, int>(2);
-        EXPECT_EQ(res, 2);
-
-        res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::MULTIPLY, true, int>(2);
-        EXPECT_EQ(res, 0);
-
-        res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::POWER, false, int>(2);
-        EXPECT_EQ(res, 1);
-
-        res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::DIVIDE, true, int>(2);
-        EXPECT_EQ(res, 0);
-
-        res = tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::INTEGER_DIVIDE, true, int>(2);
-        EXPECT_EQ(res, 0);
+        int scalar = 5;
+        auto op = tatami::make_DelayedUnaryIsometricMultiplyScalar(scalar);
+        EXPECT_TRUE(op.is_sparse());
     }
 
     {
-        std::string msg;
-        try {
-            tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::DIVIDE, true, int>(0);
-        } catch (std::exception& e) {
-            msg = e.what();
-        }
-        EXPECT_TRUE(msg.find("division by zero") != std::string::npos);
-
-        msg.clear();
-        try {
-            tatami::delayed_arithmetic_zero<tatami::ArithmeticOperation::DIVIDE, false, int>(2);
-        } catch (std::exception& e) {
-            msg = e.what();
-        }
-        EXPECT_TRUE(msg.find("division by zero") != std::string::npos);
+        int scalar = 0;
+        auto op = tatami::make_DelayedUnaryIsometricPowerScalar<false>(scalar);
+        EXPECT_FALSE(op.is_sparse());
     }
+
+    {
+        auto op = tatami::make_DelayedUnaryIsometricDivideScalar<false, int>(5);
+        tatami_test::throws_error([&]() { op.template fill<int>(true, 5); }, "division by zero");
+    }
+}
+
+TEST(DelayedUnaryIsometricArithmetic, NonFiniteMultiply) {
+    double scalar = std::numeric_limits<double>::infinity();
+    auto op = tatami::make_DelayedUnaryIsometricMultiplyScalar(scalar);
+    EXPECT_FALSE(op.is_sparse());
 }
