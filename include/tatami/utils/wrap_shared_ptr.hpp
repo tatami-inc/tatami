15c50b54407ca97896ed5730a43ce349b68fa3d1
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-08-29 21:34:56 -0700

    Switch to aliasing constructor for the wrap_shared_ptr.
    This is apparently a bit more efficient than the no-op deleter approach.

--- include/tatami/utils/wrap_shared_ptr.hpp
+++ include/tatami/utils/wrap_shared_ptr.hpp
@@ -16,17 +16,18 @@ namespace tatami {
  * Wrap a raw pointer inside a `shared_ptr`, typically to enable use of a raw `tatami::Matrix` pointer with delayed operation wrappers.
  * This enables use of delayed operations inside functions that accept a raw pointer to an externally owned `tatami::Matrix`.
  *
+ * @tparam Value_ Numeric type for the matrix value.
+ * @tparam Index_ Integer type for the row/column indices.
+ * 
  * @param ptr A pointer to a `tatami::Matrix` instance.
  * 
  * @return A shared pointer to the same object addressed by `ptr`.
  * The assumption is that `ptr` will always outlive the returned pointer.
  */
-template<typename T, typename IDX>
-std::shared_ptr<const Matrix<T, IDX> > wrap_shared_ptr(const Matrix<T, IDX>* ptr) {
-    // Using a no-op deleter in a shared pointer to get it to work with, e.g.,
-    // delayed operations without actually taking ownership of 'ptr'. This
-    // shared pointer should only be used as long as 'ptr' is alive.
-    return std::shared_ptr<const Matrix<T, IDX> >(ptr, [](const Matrix<T, IDX>*){});
+template<typename Value_, typename Index_>
+std::shared_ptr<const Matrix<Value_, Index_> > wrap_shared_ptr(const Matrix<Value_, Index_>* ptr) {
+    // Using an aliasing constructor, see https://stackoverflow.com/a/36691828
+    return std::shared_ptr<const Matrix<Value_, Index_> >(std::shared_ptr<const Matrix<Value_, Index_> >{}, ptr);
 }
 
 }
