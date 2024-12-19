e9f64c057c12c0c24c6b73134e9ed1332a92e473
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2024-12-19 00:40:16 -0800

    Added a non-const overload DelayedTranspose constructor.

--- include/tatami/other/DelayedTranspose.hpp
+++ include/tatami/other/DelayedTranspose.hpp
@@ -27,9 +27,14 @@ template<typename Value_, typename Index_>
 class DelayedTranspose : public Matrix<Value_, Index_> {
 public:
     /**
-     * @param matrix Pointer to the underlying (pre-transpose) matrix.
+     * @param matrix Pointer to the matrix to be transposed.
      */
     DelayedTranspose(std::shared_ptr<const Matrix<Value_, Index_> > matrix) : my_matrix(std::move(matrix)) {}
+
+    /**
+     * @param matrix Pointer to the matrix to be transposed.
+     */
+    DelayedTranspose(std::shared_ptr<Matrix<Value_, Index_> > matrix) : my_matrix(std::move(matrix)) {}
 
 private:
     std::shared_ptr<const Matrix<Value_, Index_> > my_matrix;
