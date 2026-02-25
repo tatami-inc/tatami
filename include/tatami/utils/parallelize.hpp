78924eeb070d95b532d54ae5968dc7a74de78e4a
LTLA <infinite.monkeys.with.keyboards@gmail.com>
2026-02-25 20:42:03 +1100

    Tightened up docs for the new parallelize function.

--- include/tatami/utils/parallelize.hpp
+++ include/tatami/utils/parallelize.hpp
@@ -18,23 +18,29 @@ namespace tatami {
 
 /**
  * Apply a function to a set of tasks in parallel, usually for iterating over a dimension of a `Matrix`.
- * By default, this uses `subpar::parallelize_range()` internally, which uses OpenMP if available and `<thread>` otherwise.
- * Advanced users can override the default parallelization mechanism by defining a `TATAMI_CUSTOM_PARALLEL` function-like macro. 
- * The macro should accept the `fun`, `tasks` and `workers` arguments as described below.
+ * By default, this uses `subpar::parallelize_range()` internally, which splits the integer sequence `[0, tasks)` into `K` non-overlapping ranges where `K <= workers`.
+ * Each range of tasks is passed to `fun()` for parallel execution using OpenMP if available and `<thread>` otherwise.
+ * Task splitting is deterministic, i.e., given the same `tasks` and `workers`, `fun()` will be called with the same combinations of worker IDs and task ranges.
+ *
+ * Advanced users can override the default parallelization mechanism by defining `TATAMI_CUSTOM_PARALLEL`.
+ * This should be a function-like macro or the name of a function that accepts the `fun`, `tasks` and `workers` arguments (see below),
+ * deterministically splits tasks into a set of ranges, calls `fun()` on each range with any parallelization scheme, and then returns the number of used workers.
+ * Once `TATAMI_CUSTOM_PARALLEL` is defined (and if `parallel_ = true`), any call to `parallelize()` will invoke the user-defined scheme instead.
  *
  * @tparam parallel_ Whether the tasks should be run in parallel.
  * If `false`, no parallelization is performed and all tasks are run on the current worker.
  * @tparam Function_ Function to be applied for a contiguous range of tasks.
  * This should accept three arguments:
- * - `worker`, the worker number executing this task range.
- *   This will be passed as an `int`.
+ * - `worker`, the worker ID executing this task range.
+ *   This will be passed as an `int` in `[0, workers)`.
  * - `task_start`, the start index of the task range.
- *   This will be passed as an `Index_`.
+ *   This will be passed as an `Index_` in `[0, tasks)`.
  * - `task_length`, the number of tasks in the task range.
- *   This will be passed as an `Index_`.
+ *   This will be passed as an `Index_` in `(0, tasks)`, i.e., it is always positive.
  * @tparam Index_ Integer type for the number of tasks.
  *
  * @param fun Function that executes a contiguous range of tasks.
+ * This will be called no more than once in each worker with a different non-overlapping range, where the union of all ranges will cover `[0, tasks)`. 
  * @param tasks Number of tasks.
  * This should be non-negative.
  * @param workers Number of workers.
@@ -42,7 +48,7 @@ namespace tatami {
  *
  * @return The number of workers (`K`) that were actually used.
  * `K` is guaranteed to be no greater than `workers`.
- * `fun()` will have been called once for each of `[0, ..., K - 1]`.
+ * `fun()` will have been called once for each of the worker IDs `[0, ..., K - 1]`.
  */
 template<bool parallel_ = true, class Function_, typename Index_>
 int parallelize(Function_ fun, const Index_ tasks, const int workers) {
